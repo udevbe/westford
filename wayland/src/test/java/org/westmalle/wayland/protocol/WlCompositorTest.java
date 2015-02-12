1ca95ba2ae2028bdf747ef8f08ab2be7ba1b0ee7
zubnix <DeRijcke.Erik@gmail.com>
2015-02-12 22:24:38 +0100

    fixup unit test so it doesnt crash the jvm

--- wayland/src/test/java/org/westmalle/wayland/protocol/WlCompositorTest.java
+++ wayland/src/test/java/org/westmalle/wayland/protocol/WlCompositorTest.java
@@ -1,25 +1,38 @@
 package org.westmalle.wayland.protocol;
 
+import com.sun.jna.Pointer;
 import org.freedesktop.wayland.server.Client;
 import org.freedesktop.wayland.server.Display;
 import org.freedesktop.wayland.server.WlCompositorResource;
+import org.freedesktop.wayland.server.jna.WaylandServerLibrary;
+import org.freedesktop.wayland.server.jna.WaylandServerLibraryMapping;
+import org.freedesktop.wayland.util.InterfaceMeta;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.westmalle.wayland.output.Compositor;
 import org.westmalle.wayland.output.RegionFactory;
 
 import static com.google.common.truth.Truth.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyInt;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({
+                        //following classes have static methods, so we have to powermock them:
+                        WaylandServerLibrary.class,
+                        InterfaceMeta.class,
+                        //following classes are final, so we have to powermock them:
                         WlSurfaceFactory.class,
                         WlRegionFactory.class,
-                        RegionFactory.class,
+                        RegionFactory.class
                 })
 public class WlCompositorTest {
 
@@ -34,15 +47,52 @@ public class WlCompositorTest {
     @Mock
     private Compositor       compositor;
 
-    @InjectMocks
-    private WlCompositor wlCompositor;
+    @Mock
+    private WaylandServerLibraryMapping waylandServerLibraryMapping;
+    @Mock
+    private InterfaceMeta               interfaceMeta;
+    @Mock
+    private Pointer                     globalPointer;
+
+    @BeforeClass
+    public static void setUpClass() {
+        PowerMockito.mockStatic(WaylandServerLibrary.class,
+                                InterfaceMeta.class);
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        when(InterfaceMeta.get(WlCompositorResource.class)).thenReturn(this.interfaceMeta);
+        when(WaylandServerLibrary.INSTANCE()).thenReturn(this.waylandServerLibraryMapping);
+        when(this.waylandServerLibraryMapping.wl_global_create(any(),
+                                                               any(),
+                                                               anyInt(),
+                                                               any(),
+                                                               any())).thenReturn(this.globalPointer);
+
+    }
 
     @Test
     public void testOnBindClient() throws Exception {
-        final WlCompositorResource wlCompositorResource = this.wlCompositor.onBindClient(mock(Client.class),
-                                                                                         1,
-                                                                                         1);
+        //given
+        final Pointer resourcePointer = mock(Pointer.class);
+        when(this.waylandServerLibraryMapping.wl_resource_create(any(),
+                                                                 any(),
+                                                                 anyInt(),
+                                                                 anyInt())).thenReturn(resourcePointer);
+        final WlCompositor wlCompositor = new WlCompositor(this.display,
+                                                           this.wlSurfaceFactory,
+                                                           this.wlRegionFactory,
+                                                           this.pixmanRegionFactory,
+                                                           this.compositor);
+
+        //when
+        final WlCompositorResource wlCompositorResource = wlCompositor.onBindClient(mock(Client.class),
+                                                                                    1,
+                                                                                    1);
+        //then
         assertThat(wlCompositorResource).isNotNull();
+        assertThat((Iterable) wlCompositor.getResources()).contains(wlCompositorResource);
     }
 
     @Test
