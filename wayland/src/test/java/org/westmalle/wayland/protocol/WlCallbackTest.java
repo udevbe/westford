344aa2ab89632a66d05fc175b49b60bdc56b8122
zubnix <DeRijcke.Erik@gmail.com>
2015-02-13 15:48:53 +0100

    add wlcallback test

---
+++ wayland/src/test/java/org/westmalle/wayland/protocol/WlCallbackTest.java
@@ -0,0 +1,50 @@
+package org.westmalle.wayland.protocol;
+
+import org.freedesktop.wayland.server.Client;
+import org.freedesktop.wayland.server.WlCallbackResource;
+import org.freedesktop.wayland.server.jna.WaylandServerLibrary;
+import org.freedesktop.wayland.server.jna.WaylandServerLibraryMapping;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({
+                    //following classes have static methods, so we have to powermock them:
+                    WaylandServerLibrary.class,
+                })
+public class WlCallbackTest {
+
+    @Mock
+    private WaylandServerLibraryMapping waylandServerLibraryMapping;
+
+    @Before
+    public void setUp() throws Exception {
+        PowerMockito.mockStatic(WaylandServerLibrary.class);
+        when(WaylandServerLibrary.INSTANCE()).thenReturn(this.waylandServerLibraryMapping);
+    }
+
+    @Test
+    public void testCreate() throws Exception {
+        //given
+        final Client client = mock(Client.class);
+        final int version = 2;
+        final int id = 3;
+        WlCallback wlCallback = new WlCallback();
+        //when
+        final WlCallbackResource wlCallbackResource = wlCallback.create(client,
+                                                                        version,
+                                                                        id);
+        //then
+        assertThat(wlCallbackResource).isNotNull();
+        assertThat(wlCallbackResource.getImplementation()).isSameAs(wlCallback);
+    }
+}
