7f8898c7d6e0ea2666de60bbf3c1c58d0b89d10d
zubnix <DeRijcke.Erik@gmail.com>
2015-02-15 22:07:13 +0100

    add sub surface tests

---
+++ wayland/src/test/java/org/westmalle/wayland/protocol/WlSubSurfaceTest.java
@@ -0,0 +1,58 @@
+package org.westmalle.wayland.protocol;
+
+import org.freedesktop.wayland.server.Client;
+import org.freedesktop.wayland.server.WlSubsurfaceResource;
+import org.freedesktop.wayland.server.WlSurfaceResource;
+import org.freedesktop.wayland.server.jna.WaylandServerLibrary;
+import org.freedesktop.wayland.server.jna.WaylandServerLibraryMapping;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({
+                        //following classes have static methods, so we have to powermock them:
+                        WaylandServerLibrary.class
+                })
+public class WlSubSurfaceTest {
+
+    @Mock
+    private WlSurfaceResource surface;
+    @Mock
+    private WlSurfaceResource parent;
+
+    @Mock
+    private WaylandServerLibraryMapping waylandServerLibraryMapping;
+
+    @Before
+    public void setUp() throws Exception {
+        PowerMockito.mockStatic(WaylandServerLibrary.class);
+        when(WaylandServerLibrary.INSTANCE()).thenReturn(this.waylandServerLibraryMapping);
+    }
+
+    @Test
+    public void testCreate() throws Exception {
+        //given
+        final Client client = mock(Client.class);
+        final int version = 1;
+        final int id = 15;
+
+        final WlSubSurface wlSubSurface = new WlSubSurface(this.surface,
+                                                           this.parent);
+        //when
+        final WlSubsurfaceResource wlSubsurfaceResource = wlSubSurface.create(client,
+                                                                              version,
+                                                                              id);
+        //then
+        assertThat(wlSubsurfaceResource).isNotNull();
+        assertThat(wlSubsurfaceResource.getImplementation()).isSameAs(wlSubSurface);
+    }
+}
