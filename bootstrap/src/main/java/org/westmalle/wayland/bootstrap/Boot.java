8dcc8e8860af59f12238eb85bde3b1a853f952c2
zubnix <DeRijcke.Erik@gmail.com>
2015-04-03 10:22:05 +0200

    make code compile with dagger2

---
+++ bootstrap/src/main/java/org/westmalle/wayland/bootstrap/Boot.java
@@ -0,0 +1,82 @@
+//Copyright 2015 Erik De Rijcke
+//
+//Licensed under the Apache License,Version2.0(the"License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//
+//http://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing,software
+//distributed under the License is distributed on an"AS IS"BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+package org.westmalle.wayland.bootstrap;
+
+import com.google.common.util.concurrent.Service;
+import com.google.common.util.concurrent.ServiceManager;
+import com.jogamp.newt.opengl.GLWindow;
+
+import org.westmalle.wayland.output.Compositor;
+import org.westmalle.wayland.output.CompositorFactory;
+import org.westmalle.wayland.output.ShmRenderer;
+import org.westmalle.wayland.output.ShmRendererFactory;
+import org.westmalle.wayland.output.gl.GLRenderEngine;
+import org.westmalle.wayland.output.gl.GLRenderEngineFactory;
+import org.westmalle.wayland.platform.newt.GLWindowFactory;
+import org.westmalle.wayland.platform.newt.GLWindowSeatFactory;
+import org.westmalle.wayland.protocol.*;
+
+import java.util.Set;
+
+public class Boot {
+
+    private void strap(final Westmalle westmalle) {
+
+        final GLWindowFactory glWindowFactory = westmalle.glWindowFactory();
+        final GLRenderEngineFactory glRenderEngineFactory = westmalle.glRenderEngineFactory();
+        final ShmRendererFactory shmRendererFactory = westmalle.shmRendererFactory();
+        final CompositorFactory compositorFactory= westmalle.compositorFactory();
+        final WlCompositorFactory wlCompositorFactory= westmalle.wlCompositorFactory();
+        final GLWindowSeatFactory glWindowSeatFactory= westmalle.glWindowSeatFactory();
+        final WlSeatFactory wlSeatFactory= westmalle.wlSeatFactory();
+        final WlShellFactory wlShellFactory= westmalle.wlShellFactory();
+        final Set<Service> services= westmalle.services();
+
+        //create an output
+        //create an X opengl enabled window
+        final GLWindow glWindow = glWindowFactory.create();
+
+        //setup our render engine
+        //create an opengl render engine that uses shm buffers and outputs to an X opengl window
+        final GLRenderEngine glRenderEngine = glRenderEngineFactory.create(glWindow);
+        //create an shm renderer that passes on shm buffers to it's render implementation
+        final ShmRenderer shmRenderer = shmRendererFactory.create(glRenderEngine);
+
+        //setup compositing
+        //create a compositor with shell and scene logic
+        final Compositor compositor = compositorFactory.create(shmRenderer);
+        //create a wayland compositor that delegates it's requests to a shell implementation.
+        final WlCompositor wlCompositor = wlCompositorFactory.create(compositor);
+
+        //setup seat
+        //create a seat that listens for input on the X opengl window and passes it on to a wayland seat.
+        //these objects will listen for input events
+        final WlSeat wlSeat = wlSeatFactory.create();
+        glWindowSeatFactory.create(glWindow,
+                                        wlSeat,
+                                        compositor);
+
+        //enable wl_shell protocol
+        wlShellFactory.create(wlCompositor);
+        //TODO enable xdg_shell protocol
+    }
+
+    public static void main(final String[] args) {
+        final Westmalle westmalle = Dagger_Westmalle.create();
+        new Boot().strap(westmalle);
+
+        //start all services, 1 thread per service & exit main thread.
+        new ServiceManager(westmalle.services()).startAsync();
+    }
+}
---
+++ bootstrap/src/main/java/org/westmalle/wayland/bootstrap/Westmalle.java
@@ -0,0 +1,54 @@
+//Copyright 2015 Erik De Rijcke
+//
+//Licensed under the Apache License,Version2.0(the"License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//
+//http://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing,software
+//distributed under the License is distributed on an"AS IS"BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+package org.westmalle.wayland.bootstrap;
+
+import com.google.common.util.concurrent.Service;
+
+import dagger.Component;
+import dagger.Module;
+
+import org.westmalle.wayland.output.CompositorFactory;
+import org.westmalle.wayland.output.ShmRendererFactory;
+import org.westmalle.wayland.output.gl.GLRenderEngineFactory;
+import org.westmalle.wayland.output.gl.OutputGLModule;
+import org.westmalle.wayland.platform.newt.GLWindowFactory;
+import org.westmalle.wayland.platform.newt.GLWindowSeatFactory;
+import org.westmalle.wayland.platform.newt.PlatformNewtModule;
+import org.westmalle.wayland.protocol.ProtocolModule;
+import org.westmalle.wayland.protocol.WlCompositorFactory;
+import org.westmalle.wayland.protocol.WlSeatFactory;
+import org.westmalle.wayland.protocol.WlShellFactory;
+
+import java.util.Set;
+
+import javax.inject.Singleton;
+
+//TODO split out project in logical components
+@Singleton
+@Component(modules = {
+        OutputGLModule.class,
+        ProtocolModule.class,
+        PlatformNewtModule.class
+})
+interface Westmalle {
+        GLWindowFactory glWindowFactory();
+        GLRenderEngineFactory glRenderEngineFactory();
+        ShmRendererFactory shmRendererFactory();
+        CompositorFactory compositorFactory();
+        WlCompositorFactory wlCompositorFactory();
+        GLWindowSeatFactory glWindowSeatFactory();
+        WlSeatFactory wlSeatFactory();
+        WlShellFactory wlShellFactory();
+        Set<Service> services();
+}
