69f83c3444da0577914b60bb3076b4a84ab7529e
zubnix <DeRijcke.Erik@gmail.com>
2015-04-07 22:14:17 +0200

    rename PlanePoint

--- wayland/src/main/java/org/westmalle/wayland/output/calc/Plane.java
+++ wayland/src/main/java/org/westmalle/wayland/output/calc/Plane.java
@@ -2,42 +2,33 @@ package org.westmalle.wayland.output.calc;
 
 import org.westmalle.wayland.output.Point;
 
+import javax.annotation.Nonnull;
 import java.util.Map;
 import java.util.Optional;
 import java.util.WeakHashMap;
-
-import javax.annotation.Nonnull;
 
 public class Plane {
 
     private final Map<Plane, Mat4> translations = new WeakHashMap<>();
 
-    public PlanePoint point(@Nonnull final Point sourcePoint) {
-        //TODO unit test
-
-        return point(Vec4.create(sourcePoint.getX(),
-                                 sourcePoint.getY(),
-                                 0,
-                                 1));
+    public PlaneLocation locate(@Nonnull final Point point) {
+        return locate(Vec4.create(point.getX(),
+                                  point.getY(),
+                                  0,
+                                  1));
     }
 
-    public PlanePoint point(@Nonnull final Vec4 source) {
-        //TODO unit test
-
-        return PlanePoint.create(source,
-                                 this);
+    public PlaneLocation locate(@Nonnull final Vec4 vec4) {
+        return PlaneLocation.create(vec4,
+                                    this);
     }
 
     public Optional<Mat4> getTranslation(@Nonnull final Plane target) {
-        //TODO unit test
-
         return Optional.ofNullable(this.translations.get(target));
     }
 
     public void setTranslation(@Nonnull final Plane target,
                                @Nonnull final Mat4 translation) {
-        //TODO unit test
-
         this.translations.put(target,
                               translation);
         target.translations.put(this,
