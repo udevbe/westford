8f1ccd1d85e2fa20eca49f8681cf160a14a3001c
zubnix <DeRijcke.Erik@gmail.com>
2015-06-08 10:29:16 +0200

    x11 impl wip

--- wayland/src/main/java/org/westmalle/wayland/x11/X11Seat.java
+++ wayland/src/main/java/org/westmalle/wayland/x11/X11Seat.java
@@ -21,15 +21,29 @@ import org.westmalle.wayland.protocol.WlSeat;
 
 import javax.annotation.Nonnull;
 
+import static org.westmalle.wayland.nativ.Libxcb.XCB_EVENT_MASK_BUTTON_PRESS;
+import static org.westmalle.wayland.nativ.Libxcb.XCB_EVENT_MASK_BUTTON_RELEASE;
+import static org.westmalle.wayland.nativ.Libxcb.XCB_EVENT_MASK_ENTER_WINDOW;
+import static org.westmalle.wayland.nativ.Libxcb.XCB_EVENT_MASK_LEAVE_WINDOW;
+import static org.westmalle.wayland.nativ.Libxcb.XCB_EVENT_MASK_POINTER_MOTION;
+
 public class X11Seat {
 
+    @Nonnull
+    private final Libxcb libxcb;
+    @Nonnull
+    private final X11Output x11Output;
     @Nonnull
     private final WlSeat      wlSeat;
     @Nonnull
     private final JobExecutor jobExecutor;
 
-    X11Seat(@Nonnull final WlSeat wlSeat,
+    X11Seat(@Nonnull final Libxcb libxcb,
+            @Nonnull final X11Output x11Output,
+            @Nonnull final WlSeat wlSeat,
             @Nonnull final JobExecutor jobExecutor) {
+        this.libxcb = libxcb;
+        this.x11Output = x11Output;
         this.wlSeat = wlSeat;
         this.jobExecutor = jobExecutor;
     }
@@ -41,15 +55,36 @@ public class X11Seat {
 
     @Subscribe
     public void handle(final xcb_button_press_event_t event) {
+
         final long  time   = event.time;
         final short button = event.detail;
 
-        this.wlSeat.getOptionalWlPointer()
-                   .ifPresent(wlPointer -> this.jobExecutor.submit(() -> wlPointer.getPointerDevice()
-                                                                                  .button(wlPointer.getResources(),
-                                                                                          (int) time,
-                                                                                          button,
-                                                                                          WlPointerButtonState.PRESSED)));
+        this.libxcb.xcb_grab_pointer(this.x11Output.getXcbConnection(),
+                                     0,
+                                     this.x11Output.getxWindow(),
+                                     XCB_EVENT_MASK_BUTTON_PRESS |
+                                     XCB_EVENT_MASK_BUTTON_RELEASE |
+                                     XCB_EVENT_MASK_POINTER_MOTION |
+                                     XCB_EVENT_MASK_ENTER_WINDOW |
+                                     XCB_EVENT_MASK_LEAVE_WINDOW,
+                                     Libxcb.XCB_GRAB_MODE_ASYNC,
+                                     Libxcb.XCB_GRAB_MODE_ASYNC,
+                                     this.x11Output.getxWindow(),
+                                     Libxcb.XCB_CURSOR_NONE,
+                                     time);
+
+        switch (button){
+            case 1:
+            case 2:
+            case 3:
+                this.wlSeat.getOptionalWlPointer()
+                        .ifPresent(wlPointer -> this.jobExecutor.submit(() -> wlPointer.getPointerDevice()
+                                .button(wlPointer.getResources(),
+                                        (int) time,
+                                        button,
+                                        WlPointerButtonState.PRESSED)));
+            default:
+        }
     }
 
     @Subscribe
@@ -59,15 +94,25 @@ public class X11Seat {
 
     @Subscribe
     public void handle(final xcb_button_release_event_t event) {
+
         final long  time   = event.time;
         final short button = event.detail;
 
-        this.wlSeat.getOptionalWlPointer()
-                   .ifPresent(wlPointer -> this.jobExecutor.submit(() -> wlPointer.getPointerDevice()
-                                                                                  .button(wlPointer.getResources(),
-                                                                                          (int) time,
-                                                                                          button,
-                                                                                          WlPointerButtonState.RELEASED)));
+        this.libxcb.xcb_ungrab_pointer(this.x11Output.getXcbConnection(),
+                                       time);
+
+        switch (button) {
+            case 1:
+            case 2:
+            case 3:
+                this.wlSeat.getOptionalWlPointer()
+                        .ifPresent(wlPointer -> this.jobExecutor.submit(() -> wlPointer.getPointerDevice()
+                                .button(wlPointer.getResources(),
+                                        (int) time,
+                                        button,
+                                        WlPointerButtonState.RELEASED)));
+            default:
+        }
     }
 
     @Subscribe
