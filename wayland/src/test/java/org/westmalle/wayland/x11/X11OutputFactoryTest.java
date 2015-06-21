e1e5c1ab4bda90b7312bdfc2a3baa62b6543ac13
zubnix <DeRijcke.Erik@gmail.com>
2015-06-21 22:22:31 +0200

    implement x11outputfactory create test

--- wayland/src/test/java/org/westmalle/wayland/x11/X11OutputFactoryTest.java
+++ wayland/src/test/java/org/westmalle/wayland/x11/X11OutputFactoryTest.java
@@ -2,6 +2,8 @@ package org.westmalle.wayland.x11;
 
 import com.sun.jna.Pointer;
 import org.freedesktop.wayland.server.Display;
+import org.freedesktop.wayland.server.EventLoop;
+import org.freedesktop.wayland.server.EventSource;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -15,6 +17,8 @@ import org.westmalle.wayland.output.OutputFactory;
 import org.westmalle.wayland.protocol.WlOutputFactory;
 
 import static org.mockito.Mockito.*;
+import static org.westmalle.wayland.nativ.LibX11xcb.XCBOwnsEventQueue;
+import static org.westmalle.wayland.nativ.Libxcb.XCB_WINDOW_CLASS_INPUT_OUTPUT;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({WlOutputFactory.class,
@@ -141,15 +145,23 @@ public class X11OutputFactoryTest {
     @Test
     public void testCreate() throws Exception {
         //given
-        final String                   xDisplayName  = ":0";
-        final int                      width         = 640;
-        final int                      height        = 480;
-        final Pointer                  xDisplay      = mock(Pointer.class);
-        final Pointer                  xcbConnection = mock(Pointer.class);
-        final Pointer                  setup         = mock(Pointer.class);
-        final xcb_screen_t.ByReference screen        = new xcb_screen_t.ByReference();
+        final EventLoop                        eventLoop     = mock(EventLoop.class);
+        final EventSource                      eventSource   = mock(EventSource.class);
+        final X11EventBus                      x11EventBus   = mock(X11EventBus.class);
+        final String                           xDisplayName  = ":0";
+        final int                              width         = 640;
+        final int                              height        = 480;
+        final Pointer                          xDisplay      = mock(Pointer.class);
+        final Pointer                          xcbConnection = mock(Pointer.class);
+        final Pointer                          setup         = mock(Pointer.class);
+        final xcb_screen_t.ByReference         screen        = new xcb_screen_t.ByReference();
+        final xcb_intern_atom_cookie_t.ByValue cookie        = new xcb_intern_atom_cookie_t.ByValue();
+        final xcb_intern_atom_reply_t          atom_reply    = new xcb_intern_atom_reply_t();
+        final int                              window        = 431;
         screen.root = 127;
         screen.root_visual = 10;
+        screen.width_in_pixels = 640;
+        screen.height_in_pixels = 480;
         final xcb_screen_iterator_t.ByValue screen_iter = new xcb_screen_iterator_t.ByValue();
         screen_iter.data = screen;
 
@@ -158,8 +170,19 @@ public class X11OutputFactoryTest {
         when(this.libxcb.xcb_connection_has_error(xcbConnection)).thenReturn(0);
         when(this.libxcb.xcb_get_setup(xcbConnection)).thenReturn(setup);
         when(this.libxcb.xcb_setup_roots_iterator(setup)).thenReturn(screen_iter);
-        when(this.libxcb.xcb_generate_id(xcbConnection)).thenReturn(431);
-
+        when(this.libxcb.xcb_generate_id(xcbConnection)).thenReturn(window);
+        when(this.display.getEventLoop()).thenReturn(eventLoop);
+        when(this.x11EventBusFactory.create(xcbConnection)).thenReturn(x11EventBus);
+        when(eventLoop.addFileDescriptor(anyInt(),
+                                         anyInt(),
+                                         any())).thenReturn(eventSource);
+        when(this.libxcb.xcb_intern_atom(eq(xcbConnection),
+                                         anyByte(),
+                                         anyShort(),
+                                         any())).thenReturn(cookie);
+        when(this.libxcb.xcb_intern_atom_reply(xcbConnection,
+                                               cookie,
+                                               null)).thenReturn(atom_reply);
         //when
         this.x11OutputFactory.create(xDisplayName,
                                      width,
@@ -167,11 +190,48 @@ public class X11OutputFactoryTest {
         //then
         verify(this.libX11).XOpenDisplay(xDisplayName);
         verify(this.libX11xcb).XGetXCBConnection(xDisplay);
+        verify(this.libX11xcb).XSetEventQueueOwner(xDisplay,
+                                                   XCBOwnsEventQueue);
         verify(this.libxcb).xcb_connection_has_error(xcbConnection);
         verify(this.libxcb).xcb_get_setup(xcbConnection);
         verify(this.libxcb).xcb_setup_roots_iterator(setup);
         verify(this.libxcb).xcb_generate_id(xcbConnection);
-        //an exception is thrown
+        verify(this.libxcb).xcb_get_file_descriptor(xcbConnection);
+        verify(this.libxcb,
+               atLeastOnce()).xcb_intern_atom(eq(xcbConnection),
+                                              anyByte(),
+                                              anyShort(),
+                                              any());
+        verify(this.libxcb,
+               atLeastOnce()).xcb_intern_atom_reply(xcbConnection,
+                                                    cookie,
+                                                    null);
+        verify(this.libxcb,
+               atLeastOnce()).xcb_change_property(eq(xcbConnection),
+                                                  anyByte(),
+                                                  eq(window),
+                                                  anyInt(),
+                                                  anyInt(),
+                                                  anyByte(),
+                                                  anyInt(),
+                                                  any());
+        verify(this.libxcb).xcb_create_window(eq(xcbConnection),
+                                              anyByte(),
+                                              eq(window),
+                                              eq(screen.root),
+                                              anyShort(),
+                                              anyShort(),
+                                              eq((short) width),
+                                              eq((short) height),
+                                              anyShort(),
+                                              eq((short) XCB_WINDOW_CLASS_INPUT_OUTPUT),
+                                              eq(screen.root_visual),
+                                              anyInt(),
+                                              any());
+        verify(this.libxcb).xcb_map_window(xcbConnection,
+                                           window);
+        verify(this.libxcb).xcb_flush(xcbConnection);
+
         verifyNoMoreInteractions(this.libX11);
         verifyNoMoreInteractions(this.libX11xcb);
         verifyNoMoreInteractions(this.libxcb);
