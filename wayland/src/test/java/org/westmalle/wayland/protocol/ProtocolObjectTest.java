67c118b34b10619bd20523710ac9b74adc9b0f25
zubnix <DeRijcke.Erik@gmail.com>
2015-06-30 22:31:57 +0200

    add test for bug where resource was not properly cleaned up

--- wayland/src/test/java/org/westmalle/wayland/protocol/ProtocolObjectTest.java
+++ wayland/src/test/java/org/westmalle/wayland/protocol/ProtocolObjectTest.java
@@ -1,12 +1,14 @@
 package org.westmalle.wayland.protocol;
 
 import org.freedesktop.wayland.server.Client;
+import org.freedesktop.wayland.server.Listener;
 import org.freedesktop.wayland.server.Resource;
 import org.freedesktop.wayland.server.jna.WaylandServerLibrary;
 import org.freedesktop.wayland.server.jna.WaylandServerLibraryMapping;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
@@ -15,8 +17,7 @@ import org.powermock.modules.junit4.PowerMockRunner;
 import java.util.Optional;
 
 import static com.google.common.truth.Truth.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(WaylandServerLibrary.class)
@@ -112,5 +113,26 @@ public class ProtocolObjectTest {
         assertThat((Iterable) protocolObject.getResources()).contains(resource1);
         assertThat((Iterable) protocolObject.getResources()).contains(resource2);
         assertThat((Iterable) protocolObject.getResources()).hasSize(3);
+    }
+
+    @Test
+    public void testDestroyed() {
+        //given
+        final Client                      client         = mock(Client.class);
+        final ProtocolObject<Resource<?>> protocolObject = new ProtocolObjectDummy();
+        //when
+        final Resource<?> resource = protocolObject.add(client,
+                                                        1,
+                                                        1);
+        //then
+        final ArgumentCaptor<Listener> listenerArgumentCaptor = ArgumentCaptor.forClass(Listener.class);
+        verify(resource).addDestroyListener(listenerArgumentCaptor.capture());
+
+        //and when
+        final Listener listener = listenerArgumentCaptor.getValue();
+        listener.handle();
+
+        //then
+        assertThat((Iterable<Resource<?>>) protocolObject.getResources()).doesNotContain(resource);
     }
 }
