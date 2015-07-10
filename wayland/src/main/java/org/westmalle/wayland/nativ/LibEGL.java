96efa6f7a36a8cf8bd4c5952b6f88d442a41b2a0
zubnix <DeRijcke.Erik@gmail.com>
2015-07-10 10:16:41 +0200

    quick fix for #2

--- wayland/src/main/java/org/westmalle/wayland/nativ/LibEGL.java
+++ wayland/src/main/java/org/westmalle/wayland/nativ/LibEGL.java
@@ -13,66 +13,102 @@
 //limitations under the License.
 package org.westmalle.wayland.nativ;
 
+import com.sun.jna.Function;
 import com.sun.jna.Native;
 import com.sun.jna.Pointer;
 
 public class LibEGL {
 
-    public static final Pointer EGL_NO_DISPLAY              = Pointer.createConstant(0);
-    public static final int     EGL_PLATFORM_ANDROID_KHR    = 0x3141;
-    public static final int     EGL_PLATFORM_GBM_KHR        = 0x31D7;
-    public static final int     EGL_PLATFORM_WAYLAND_KHR    = 0x31D8;
-    public static final int     EGL_PLATFORM_X11_KHR        = 0x31D5;
-    public static final int     EGL_PLATFORM_X11_SCREEN_KHR = 0x31D6;
-    public static final int     EGL_PLATFORM_GBM_MESA       = 0x31D7;
-    public static final int     EGL_OPENGL_ES_API           = 0x30A0;
-    public static final int     EGL_OPENGL_API              = 0x30A2;
-    public static final int     EGL_WAYLAND_BUFFER_WL       = 0x31D5;
-    public static final int     EGL_WAYLAND_PLANE_WL        = 0x31D6;
-    public static final int     EGL_TEXTURE_Y_U_V_WL        = 0x31D7;
-    public static final int     EGL_TEXTURE_Y_UV_WL         = 0x31D8;
-    public static final int     EGL_TEXTURE_Y_XUXV_WL       = 0x31D9;
-    public static final int     EGL_TEXTURE_FORMAT          = 0x3080;
-    public static final int     EGL_WAYLAND_Y_INVERTED_WL   = 0x31DB;
-    public static final int     EGL_COLOR_BUFFER_TYPE       = 0x303F;
-    public static final int     EGL_RGB_BUFFER              = 0x308E;
-    public static final int     EGL_BUFFER_SIZE             = 0x3020;
-    public static final int     EGL_RED_SIZE                = 0x3024;
-    public static final int     EGL_GREEN_SIZE              = 0x3023;
-    public static final int     EGL_BLUE_SIZE               = 0x3022;
-    public static final int     EGL_ALPHA_SIZE              = 0x3021;
-    public static final int     EGL_DEPTH_SIZE              = 0x3025;
-    public static final int     EGL_STENCIL_SIZE            = 0x3026;
-    public static final int     EGL_SAMPLE_BUFFERS          = 0x3032;
-    public static final int     EGL_SAMPLES                 = 0x3031;
-    public static final int     EGL_SURFACE_TYPE            = 0x3033;
-    public static final int     EGL_WINDOW_BIT              = 0x0004;
-    public static final int     EGL_RENDERABLE_TYPE         = 0x3040;
-    public static final int     EGL_OPENGL_ES2_BIT          = 0x0004;
-    public static final int     EGL_NONE                    = 0x3038;
-    public static final int     EGL_CONTEXT_CLIENT_VERSION  = 0x3098;
-    public static final Pointer EGL_NO_CONTEXT              = Pointer.createConstant(0);
-    public static final int     EGL_BACK_BUFFER             = 0x3084;
-    public static final int     EGL_RENDER_BUFFER           = 0x3086;
-    public static final int     EGL_SINGLE_BUFFER           = 0x3085;
-
     static {
         Native.register("EGL");
     }
 
+    public static final Pointer EGL_NO_DISPLAY = Pointer.createConstant(0);
+    public static final int EGL_PLATFORM_ANDROID_KHR = 0x3141;
+    public static final int EGL_PLATFORM_GBM_KHR = 0x31D7;
+    public static final int EGL_PLATFORM_WAYLAND_KHR = 0x31D8;
+    public static final int EGL_PLATFORM_X11_KHR = 0x31D5;
+    public static final int EGL_PLATFORM_X11_SCREEN_KHR = 0x31D6;
+    public static final int EGL_PLATFORM_GBM_MESA = 0x31D7;
+    public static final int EGL_OPENGL_ES_API = 0x30A0;
+    public static final int EGL_OPENGL_API = 0x30A2;
+    public static final int EGL_WAYLAND_BUFFER_WL = 0x31D5;
+    public static final int EGL_WAYLAND_PLANE_WL = 0x31D6;
+    public static final int EGL_TEXTURE_Y_U_V_WL = 0x31D7;
+    public static final int EGL_TEXTURE_Y_UV_WL = 0x31D8;
+    public static final int EGL_TEXTURE_Y_XUXV_WL = 0x31D9;
+    public static final int EGL_TEXTURE_FORMAT = 0x3080;
+    public static final int EGL_WAYLAND_Y_INVERTED_WL = 0x31DB;
+    public static final int EGL_COLOR_BUFFER_TYPE = 0x303F;
+    public static final int EGL_RGB_BUFFER = 0x308E;
+    public static final int EGL_BUFFER_SIZE = 0x3020;
+    public static final int EGL_RED_SIZE = 0x3024;
+    public static final int EGL_GREEN_SIZE = 0x3023;
+    public static final int EGL_BLUE_SIZE = 0x3022;
+    public static final int EGL_ALPHA_SIZE = 0x3021;
+    public static final int EGL_DEPTH_SIZE = 0x3025;
+    public static final int EGL_STENCIL_SIZE = 0x3026;
+    public static final int EGL_SAMPLE_BUFFERS = 0x3032;
+    public static final int EGL_SAMPLES = 0x3031;
+    public static final int EGL_SURFACE_TYPE = 0x3033;
+    public static final int EGL_WINDOW_BIT = 0x0004;
+    public static final int EGL_RENDERABLE_TYPE = 0x3040;
+    public static final int EGL_OPENGL_ES2_BIT = 0x0004;
+    public static final int EGL_NONE = 0x3038;
+    public static final int EGL_CONTEXT_CLIENT_VERSION = 0x3098;
+    public static final Pointer EGL_NO_CONTEXT = Pointer.createConstant(0);
+    public static final int EGL_BACK_BUFFER = 0x3084;
+    public static final int EGL_RENDER_BUFFER = 0x3086;
+    public static final int EGL_SINGLE_BUFFER = 0x3085;
+    public static final int EGL_VENDOR = 0x3053;
+    public static final int EGL_VERSION = 0x3054;
+    public static final int EGL_EXTENSIONS = 0x3055;
+    public static final int EGL_CLIENT_APIS = 0x308D;
+
+    //FIXME move loading & checking of extension to a LibEGL factory
+    private Function eglCreatePlatformWindowSurfaceEXT = loadFunction("eglCreatePlatformWindowSurfaceEXT");
+    private Function eglGetPlatformDisplayEXT = loadFunction("eglGetPlatformDisplayEXT");
+
     LibEGL() {
     }
 
-    public native boolean eglBindWaylandDisplayWL(Pointer dpy,
-                                                  Pointer display);
+    private Function loadFunction(String name) {
+        final NativeString procname = new NativeString(name);
+        Pointer functionPointer = eglGetProcAddress(procname.getPointer());
+        return Function.getFunction(functionPointer);
+    }
 
-    public native boolean eglUnbindWaylandDisplayWL(Pointer dpy,
-                                                    Pointer display);
+//    public boolean eglBindWaylandDisplayWL(Pointer dpy,
+//                                           Pointer display) {
+//        final Function function = getFunction("eglBindWaylandDisplayWL");
+//        return (boolean) function.invoke(boolean.class,
+//                                         new Object[]{
+//                                                 dpy,
+//                                                 display
+//                                         });
+//    }
 
-    public native boolean eglQueryWaylandBufferWL(Pointer dpy,
-                                                  Pointer buffer,
-                                                  int attribute,
-                                                  Pointer value);
+//    public boolean eglUnbindWaylandDisplayWL(Pointer dpy,
+//                                             Pointer display) {
+//        final Function function = getFunction("eglUnbindWaylandDisplayWL");
+//        return (boolean) function.invoke(boolean.class, new Object[]{
+//                dpy,
+//                display
+//        });
+//    }
+
+//    public boolean eglQueryWaylandBufferWL(Pointer dpy,
+//                                           Pointer buffer,
+//                                           int attribute,
+//                                           Pointer value) {
+//        final Function function = getFunction("eglQueryWaylandBufferWL");
+//        return (boolean) function.invoke(boolean.class, new Object[]{
+//                dpy,
+//                buffer,
+//                attribute,
+//                value
+//        });
+//    }
 
     public native boolean eglInitialize(Pointer dpy,
                                         Pointer major,
@@ -95,14 +131,34 @@ public class LibEGL {
                                            Pointer share_context,
                                            Pointer attrib_list);
 
-    public native Pointer eglGetPlatformDisplayEXT(int platform,
-                                                   Pointer native_display,
-                                                   Pointer attrib_list);
+    public Pointer eglGetPlatformDisplayEXT(final int platform,
+                                            final Pointer native_display,
+                                            final Pointer attrib_list) {
+        return (Pointer) this.eglGetPlatformDisplayEXT.invoke(Pointer.class,
+                                                              new Object[]{
+                                                                      platform,
+                                                                      native_display,
+                                                                      attrib_list
+                                                              });
+    }
 
-    public native Pointer eglCreatePlatformWindowSurfaceEXT(Pointer dpy,
-                                                            Pointer config,
-                                                            Pointer native_window,
-                                                            Pointer attrib_list);
+    public native Pointer eglGetProcAddress(Pointer procname);
+
+    public native Pointer eglQueryString(Pointer dpy,
+                                         int name);
+
+    public Pointer eglCreatePlatformWindowSurfaceEXT(Pointer dpy,
+                                                     Pointer config,
+                                                     Pointer native_window,
+                                                     Pointer attrib_list) {
+        return (Pointer) this.eglCreatePlatformWindowSurfaceEXT.invoke(Pointer.class,
+                                                                       new Object[]{
+                                                                               dpy,
+                                                                               config,
+                                                                               native_window,
+                                                                               attrib_list
+                                                                       });
+    }
 
     public native boolean eglChooseConfig(Pointer dpy,
                                           Pointer attrib_list,
