335f82f4abbccf907585f4e433d891387e5ec611
zubnix <DeRijcke.Erik@gmail.com>
2015-07-30 10:15:20 +0200

    add docs

--- wayland/src/main/java/org/westmalle/wayland/nativ/libxkbcommon/Libxkbcommon.java
+++ wayland/src/main/java/org/westmalle/wayland/nativ/libxkbcommon/Libxkbcommon.java
@@ -3,6 +3,9 @@ package org.westmalle.wayland.nativ.libxkbcommon;
 
 import com.sun.jna.Pointer;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
 public class Libxkbcommon {
 
     /**
@@ -71,22 +74,113 @@ public class Libxkbcommon {
     public static final int XKB_STATE_LEDS             = (1 << 8);
 
 
+    /**
+     * Create a new context.
+     *
+     * @param flags Optional flags for the context, or 0.
+     *
+     * @return A new context, or null on failure.
+     */
+    @Nullable
     public native Pointer xkb_context_new(int flags);
 
-    public native Pointer xkb_keymap_new_from_names(Pointer context,
+    /**
+     * Create a keymap from RMLVO names.
+     * <p/>
+     * The primary keymap entry point: creates a new XKB keymap from a set of RMLVO (Rules + Model + Layouts + Variants + Options) names.
+     *
+     * @param context The context in which to create the keymap.
+     * @param names   The RMLVO names to use.
+     * @param flags   Optional flags for the keymap, or 0.
+     *
+     * @return A keymap compiled according to the RMLVO names, or null if the compilation failed.
+     */
+    @Nullable
+    public native Pointer xkb_keymap_new_from_names(@Nonnull Pointer context,
                                                     Pointer names,
                                                     int flags);
 
-    public native Pointer xkb_state_new(Pointer keymap);
+    /**
+     * Create a new keyboard state object.
+     *
+     * @param keymap The keymap which the state will use.
+     *
+     * @return A new keyboard state object, or null on failure.
+     */
+    @Nullable
+    public native Pointer xkb_state_new(@Nonnull Pointer keymap);
 
-    public native int xkb_state_update_key(Pointer state,
+    /**
+     * Update the keyboard state to reflect a given key being pressed or released.
+     * <p/>
+     * This entry point is intended for programs which track the keyboard state explictly (like an evdev client).
+     * If the state is serialized to you by a master process (like a Wayland compositor) using functions like
+     * xkb_state_serialize_mods(), you should use xkb_state_update_mask() instead. The two functins should not
+     * generally be used together.
+     * <p/>
+     * A series of calls to this function should be consistent; that is, a call with XKB_KEY_DOWN for a key should be
+     * matched by an XKB_KEY_UP; if a key is pressed twice, it should be released twice; etc. Otherwise (e.g. due to
+     * missed input events), situations like "stuck modifiers" may occur.
+     * <p/>
+     * This function is often used in conjunction with the function xkb_state_key_get_syms() (or
+     * xkb_state_key_get_one_sym()), for example, when handling a key event. In this case, you should prefer to get
+     * the keysyms before updating the key, such that the keysyms reported for the key event are not affected by the
+     * event itself. This is the conventional behavior.
+     *
+     * @param state
+     * @param key
+     * @param direction
+     *
+     * @return A mask of state components that have changed as a result of the update. If nothing in the state has
+     * changed, returns 0.
+     */
+    public native int xkb_state_update_key(@Nonnull Pointer state,
                                            int key,
                                            int direction);
 
-    public native int xkb_keymap_key_repeats(Pointer keymap,
+    /**
+     * Determine whether a key should repeat or not.
+     * <p/>
+     * A keymap may specify different repeat behaviors for different keys. Most keys should generally exhibit repeat
+     * behavior; for example, holding the 'a' key down in a text editor should normally insert a single 'a' character
+     * every few milliseconds, until the key is released. However, there are keys which should not or do not need to be
+     * repeated. For example, repeating modifier keys such as Left/Right Shift or Caps Lock is not generally useful or
+     * desired.
+     *
+     * @param keymap
+     * @param key
+     *
+     * @return 1 if the key should repeat, 0 otherwise.
+     */
+    public native int xkb_keymap_key_repeats(@Nonnull Pointer keymap,
                                              int key);
 
-    public native int xkb_state_update_mask(Pointer state,
+    /**
+     * Update a keyboard state from a set of explicit masks.
+     * <p/>
+     * This entry point is intended for window systems and the like, where a master process holds an xkb_state, then
+     * serializes it over a wire protocol, and clients then use the serialization to feed in to their own xkb_state.
+     * <p/>
+     * All parameters must always be passed, or the resulting state may be incoherent.
+     * <p/>
+     * The serialization is lossy and will not survive round trips; it must only be used to feed slave state objects,
+     * and must not be used to update the master state.
+     * <p/>
+     * If you do not fit the description above, you should use {@link Libxkbcommon#xkb_state_update_key} instead. The
+     * two functions should not generally be used together.
+     *
+     * @param state
+     * @param depressed_mods
+     * @param latched_mods
+     * @param locked_mods
+     * @param depressed_layout
+     * @param latched_layout
+     * @param locked_layout
+     *
+     * @return A mask of state components that have changed as a result of the update. If nothing in the state has
+     * changed, returns 0.
+     */
+    public native int xkb_state_update_mask(@Nonnull Pointer state,
                                             int depressed_mods,
                                             int latched_mods,
                                             int locked_mods,
@@ -94,9 +188,24 @@ public class Libxkbcommon {
                                             int latched_layout,
                                             int locked_layout);
 
-    public native void xkb_state_unref(Pointer state);
+    /**
+     * Release a reference on a keyboard state object, and possibly free it.
+     *
+     * @param state The state. If it is null, this function does nothing.
+     */
+    public native void xkb_state_unref(@Nullable Pointer state);
 
-    public native void xkb_keymap_unref(Pointer keymap);
+    /**
+     * Release a reference on a keymap, and possibly free it.
+     *
+     * @param keymap The keymap. If it is null, this function does nothing.
+     */
+    public native void xkb_keymap_unref(@Nullable Pointer keymap);
 
-    public native void xkb_context_unref(Pointer context);
+    /**
+     * Release a reference on a context, and possibly free it.
+     *
+     * @param context The context. If it is null, this function does nothing.
+     */
+    public native void xkb_context_unref(@Nullable Pointer context);
 }
