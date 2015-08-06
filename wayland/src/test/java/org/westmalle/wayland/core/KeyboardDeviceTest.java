ae7311aeff3e89b7adc602aaccf9a827192d5786
zubnix <DeRijcke.Erik@gmail.com>
2015-08-06 13:50:14 +0200

    add skeleton keyboard device tests

---
+++ wayland/src/test/java/org/westmalle/wayland/core/KeyboardDeviceTest.java
@@ -0,0 +1,21 @@
+package org.westmalle.wayland.core;
+
+import org.junit.Test;
+
+public class KeyboardDeviceTest {
+
+    @Test
+    public void testKey() throws Exception {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void testSetFocus() throws Exception {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void testUpdateKeymap() throws Exception {
+        throw new UnsupportedOperationException();
+    }
+}
