44db6366592cd0221372d41cd9dc797b2fd44d01
zubnix <DeRijcke.Erik@gmail.com>
2015-08-06 10:18:42 +0200

    fix mmap flags

--- wayland/src/main/java/org/westmalle/wayland/nativ/libc/Libc.java
+++ wayland/src/main/java/org/westmalle/wayland/nativ/libc/Libc.java
@@ -234,31 +234,37 @@ public class Libc {
     public static final int ERANGE  = 34;
 
     /**
-     * pages can be read
+     * Page can be read.
      */
-    public static final int PROT_READ  = 0x04;
+    public static final int PROT_READ      = 0x1;
     /**
-     * pages can be written
+     * Page can be written.
      */
-    public static final int PROT_WRITE = 0x02;
+    public static final int PROT_WRITE     = 0x2;
     /**
-     * pages can be executed
+     * Page can be executed.
      */
-    public static final int PROT_EXEC  = 0x01;
-
+    public static final int PROT_EXEC      = 0x4;
     /**
-     * "copy" region at mmap time
+     * Page can not be accessed.
      */
-    public static final int MAP_COPY    = 0x0020;
+    public static final int PROT_NONE      = 0x0;
     /**
-     * share changes
+     * Extend change to start of  growsdown vma (mprotect only).
      */
-    public static final int MAP_SHARED  = 0x0010;
+    public static final int PROT_GROWSDOWN = 0x01000000;
     /**
-     * changes are private
+     * Extend change to start of  growsup vma (mprotect only).
      */
-    public static final int MAP_PRIVATE = 0x0000;
-
+    public static final int PROT_GROWSUP   = 0x02000000;
+    /**
+     * Share changes.
+     */
+    public static final int MAP_SHARED     = 0x01;
+    /**
+     * Changes are private.
+     */
+    public static final int MAP_PRIVATE    = 0x02;
     //-1
     public static final Pointer MAP_FAILED = Pointer.createConstant(0xFFFFFFFF);
 
