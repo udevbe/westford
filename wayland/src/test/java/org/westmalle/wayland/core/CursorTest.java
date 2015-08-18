40159db6fdfcf1c0d8295568430a6ff9792fd61f
zubnix <DeRijcke.Erik@gmail.com>
2015-08-18 16:45:54 +0200

    implement cursor unit tests

--- wayland/src/test/java/org/westmalle/wayland/core/CursorTest.java
+++ wayland/src/test/java/org/westmalle/wayland/core/CursorTest.java
@@ -1,21 +1,67 @@
 package org.westmalle.wayland.core;
 
+import org.freedesktop.wayland.server.WlBufferResource;
+import org.freedesktop.wayland.server.WlSurfaceResource;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.westmalle.wayland.protocol.WlSurface;
 
+import java.util.Optional;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
 public class CursorTest {
 
-    @Test
-    public void testHide() throws Exception {
-        throw new UnsupportedOperationException();
+    @Mock
+    private Surface   surface;
+    @Mock
+    private WlSurface wlSurface;
+
+    @Mock
+    private WlSurfaceResource wlSurfaceResource;
+    @Mock
+    private Point             hotspot;
+    @InjectMocks
+    private Cursor            cursor;
+
+    @Before
+    public void setUp() {
+        when(this.wlSurfaceResource.getImplementation()).thenReturn(this.wlSurface);
+        when(this.wlSurface.getSurface()).thenReturn(this.surface);
     }
 
     @Test
-    public void testShow() throws Exception {
-        throw new UnsupportedOperationException();
+    public void testHide() throws Exception {
+        //given
+        final SurfaceState surfaceState = SurfaceState.builder()
+                                                      .build();
+        when(this.surface.getState()).thenReturn(surfaceState);
+        //when
+        this.cursor.hide();
+        //then
+        verify(this.surface).setState(eq(SurfaceState.builder()
+                                                     .buffer(Optional.<WlBufferResource>empty())
+                                                     .build()));
+        assertThat(this.cursor.isHidden()).isTrue();
     }
 
     @Test
     public void testUpdatePosition() throws Exception {
-        throw new UnsupportedOperationException();
+        //given
+        final Point point = Point.create(123,
+                                         456);
+        //when
+        this.cursor.updatePosition(point);
+        //then
+        verify(this.surface).setPosition(eq(Point.create(123,
+                                                         456)));
     }
 }
