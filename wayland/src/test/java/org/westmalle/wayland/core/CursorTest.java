eae203b9e72b54115079ad30fdae23ee27fc1b00
zubnix <DeRijcke.Erik@gmail.com>
2015-08-18 13:49:28 +0200

    add cursor unit tests skeletons

---
+++ wayland/src/test/java/org/westmalle/wayland/core/CursorTest.java
@@ -0,0 +1,21 @@
+package org.westmalle.wayland.core;
+
+import org.junit.Test;
+
+public class CursorTest {
+
+    @Test
+    public void testHide() throws Exception {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void testShow() throws Exception {
+        throw new UnsupportedOperationException();
+    }
+
+    @Test
+    public void testUpdatePosition() throws Exception {
+        throw new UnsupportedOperationException();
+    }
+}
