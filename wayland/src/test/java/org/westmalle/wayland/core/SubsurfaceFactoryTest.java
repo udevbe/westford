fa43e772e7c28e0a4fea47a4684bd89296c5bc7e
zubnix <DeRijcke.Erik@gmail.com>
2015-08-23 08:04:51 +0200

    add subsurfacefactory unit test skeleton methods

---
+++ wayland/src/test/java/org/westmalle/wayland/core/SubsurfaceFactoryTest.java
@@ -0,0 +1,39 @@
+//Copyright 2015 Erik De Rijcke
+//
+//Licensed under the Apache License,Version2.0(the"License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//
+//http://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing,software
+//distributed under the License is distributed on an"AS IS"BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+package org.westmalle.wayland.core;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SubsurfaceFactoryTest {
+
+    @Test
+    public void testCreate() throws Exception {
+        //TODO
+
+        //given: subsurface factory, parent surface, surface
+        //when: create is called
+        //then: a subsurface is created
+        // and the subsurface has position 0,0
+        // and the subsurface is placed at the top of the subsurface stack
+        //and when: the parent position is changed
+        //then: the subsurface position is applied
+        //and when: the parent commit is called
+        //then: the subsurface parent commit hook is called
+        //and when: the surface is destroyed
+        //then: the surface is removed from the subsurface stack
+        // and the surface is removed from the pending subsurface stack
+    }
+}
--- wayland/src/test/java/org/westmalle/wayland/core/SubsurfaceTest.java
+++ wayland/src/test/java/org/westmalle/wayland/core/SubsurfaceTest.java
@@ -1,3 +1,16 @@
+//Copyright 2015 Erik De Rijcke
+//
+//Licensed under the Apache License,Version2.0(the"License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//
+//http://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing,software
+//distributed under the License is distributed on an"AS IS"BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
 package org.westmalle.wayland.core;
 
 import org.junit.Test;
@@ -7,6 +20,8 @@ public class SubsurfaceTest {
 
     @Test
     public void testSetPosition() throws Exception {
+        //TODO
+
         //given: a subsurface
         //when: a new position is set
         //then: the position is stored but not applied.
@@ -14,6 +29,8 @@ public class SubsurfaceTest {
 
     @Test
     public void testSetPositionInert() throws Exception {
+        //TODO
+
         //given: an inert subsurface
         //when: a new position is set
         //then: nothing happens
@@ -21,6 +38,8 @@ public class SubsurfaceTest {
 
     @Test
     public void testApplyPosition() throws Exception {
+        //TODO
+
         //given: a subsurface, a parent surface
         //when: the subsurface position is applied
         //then: the subsurface position is applied based on the parent surface space.
@@ -28,6 +47,8 @@ public class SubsurfaceTest {
 
     @Test
     public void testApplyPositionInert() throws Exception {
+        //TODO
+
         //given: an inert subsurface
         //when: the subsurface position is applied
         //then: nothing happens
@@ -35,6 +56,8 @@ public class SubsurfaceTest {
 
     @Test
     public void testBeforeCommitSync() throws Exception {
+        //TODO
+
         //given: a subsurface in sync mode
         //when: the before commit hook is called
         //then: the pending surface state is restored
@@ -42,6 +65,8 @@ public class SubsurfaceTest {
 
     @Test
     public void testBeforeCommitDesync() throws Exception {
+        //TODO
+
         //given: a subsurface in desync mode
         //when: the before commit hook is called
         //then: nothing happens
@@ -49,6 +74,8 @@ public class SubsurfaceTest {
 
     @Test
     public void testBeforeCommitInert() throws Exception {
+        //TODO
+
         //given: an inert subsurface
         //when: the before commit hook is called
         //then: nothing happens
@@ -56,6 +83,8 @@ public class SubsurfaceTest {
 
     @Test
     public void testCommitSync() throws Exception {
+        //TODO
+
         //given: a subsurface in sync mode
         //when: the commit hook is called
         //then: the surface new state is cached and the surface is reset to the pre-commit state.
@@ -63,6 +92,8 @@ public class SubsurfaceTest {
 
     @Test
     public void testCommitDesync() throws Exception {
+        //TODO
+
         //given: a subsurface in desync mode
         //when: the commit hook is called
         //then: the cached state is updated to the current surface state
@@ -70,6 +101,8 @@ public class SubsurfaceTest {
 
     @Test
     public void testCommitInert() throws Exception {
+        //TODO
+
         //given: an inert subsurface
         //when: the commit hook is called
         //then: nothing happens
@@ -77,6 +110,8 @@ public class SubsurfaceTest {
 
     @Test
     public void testParentCommitSync() throws Exception {
+        //TODO
+
         //given: a subsurface in sync mode, a parent surface
         //when: the parent commit hook is called
         //then: the cached state is applied on the surface, surface position is applied
@@ -84,6 +119,8 @@ public class SubsurfaceTest {
 
     @Test
     public void testParentCommitSyncNoSurfaceStateUpdate() throws Exception {
+        //TODO
+
         //given: a subsurface in sync mode, a parent surface, no surface state changes
         //when: the parent commit hook is called
         //then: no surface state update is applied, surface position is applied
@@ -91,6 +128,8 @@ public class SubsurfaceTest {
 
     @Test
     public void testParentCommitDesync() throws Exception {
+        //TODO
+
         //given: a subsurface in desync mode, a parent surface
         //when: the parent commit hook is called
         //then: no surface state update is applied, surface position is applied
@@ -98,6 +137,8 @@ public class SubsurfaceTest {
 
     @Test
     public void testParentCommitInert() throws Exception {
+        //TODO
+
         //given: an inert subsurface
         //when: the parent commit hook is called
         //then: nothing happens
@@ -105,6 +146,8 @@ public class SubsurfaceTest {
 
     @Test
     public void testSetSyncInert() throws Exception {
+        //TODO
+
         //given: an inert subsurface
         //when: set sync is called
         //then: nothing happens
@@ -112,6 +155,8 @@ public class SubsurfaceTest {
 
     @Test
     public void testSetDesyncInert() throws Exception {
+        //TODO
+
         //given: an inert subsurface
         //when: set desync is called
         //then: nothing happens
@@ -119,6 +164,8 @@ public class SubsurfaceTest {
 
     @Test
     public void testAbove() throws Exception {
+        //TODO
+
         //given: a subsurface, a sibling or parents surface
         //when: set above is called
         //then: the pending subsurface stack is updated.
@@ -126,6 +173,8 @@ public class SubsurfaceTest {
 
     @Test
     public void testAboveInert() throws Exception {
+        //TODO
+
         //given: an inert subsurface
         //when: set above is called
         //then: nothing happens
@@ -133,6 +182,8 @@ public class SubsurfaceTest {
 
     @Test
     public void testBelow() throws Exception {
+        //TODO
+
         //given: a subsurface, a sibling or parents surface
         //when: set below is called
         //then: the pending subsurface stack is updated.
@@ -140,6 +191,8 @@ public class SubsurfaceTest {
 
     @Test
     public void testBelowInert() throws Exception {
+        //TODO
+
         //given: an inert subsurface
         //when: set below is called
         //then: nothing happens
@@ -147,6 +200,8 @@ public class SubsurfaceTest {
 
     @Test
     public void testIsInert() throws Exception {
+        //TODO
+
         //given: a subsurface, a parent surface
         //when: the parent surface is destroyed
         //then: the subsurface becomes inert.
