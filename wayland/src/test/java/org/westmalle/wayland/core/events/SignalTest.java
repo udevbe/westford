36792a94f409f9efe51796b82ea234f1be4510ee
zubnix <DeRijcke.Erik@gmail.com>
2015-09-14 14:50:01 +0200

    add unit test for signal implementation

---
+++ wayland/src/test/java/org/westmalle/wayland/core/events/SignalTest.java
@@ -0,0 +1,55 @@
+package org.westmalle.wayland.core.events;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SignalTest {
+
+    @InjectMocks
+    private Signal<Object, Slot<Object>> signal;
+
+    @Test
+    public void testEmit() throws Exception {
+        //given
+        final Object       event = new Object();
+        final Slot<Object> slot  = mock(Slot.class);
+        this.signal.connect(slot);
+
+        //when
+        this.signal.emit(event);
+
+        //then
+        verify(slot).handle(event);
+    }
+
+    @Test
+    public void testEmitUpdateSlot() throws Exception {
+        //given
+        final Object       event = new Object();
+        final Slot<Object> slot  = mock(Slot.class);
+        doAnswer(invocation -> {
+            this.signal.disconnect(slot);
+            return null;
+        }).when(slot)
+          .handle(event);
+        this.signal.connect(slot);
+
+        //when
+        this.signal.emit(event);
+
+        //then
+        verify(slot).handle(event);
+
+        //and when
+        this.signal.emit(event);
+        verifyNoMoreInteractions(slot);
+    }
+}
