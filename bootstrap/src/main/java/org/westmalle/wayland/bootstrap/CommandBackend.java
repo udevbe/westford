47bf59736eb7262b8ca5484b7d008aa41e3faa8d
zubnix <DeRijcke.Erik@gmail.com>
2015-11-06 11:50:05 +0100

    use default empty backend to avoid NPE

--- bootstrap/src/main/java/org/westmalle/wayland/bootstrap/CommandBackend.java
+++ bootstrap/src/main/java/org/westmalle/wayland/bootstrap/CommandBackend.java
@@ -2,8 +2,11 @@ package org.westmalle.wayland.bootstrap;
 
 import com.beust.jcommander.Parameter;
 
+import javax.annotation.Nonnull;
+
 
 public class CommandBackend {
     @Parameter(names = "-back_end")
-    public String backend;
+    @Nonnull
+    public String backend = "";
 }
