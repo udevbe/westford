ef67df6c88e05584a0744d00df22bb01a762f3da
Erik De Rijcke <derijcke.erik@gmail.com>
2016-05-02 23:19:27 +0200

    port x11 egl output unit tests to jaccall

--- wayland/src/test/java/org/westmalle/wayland/x11/X11EglOutputTest.java
+++ wayland/src/test/java/org/westmalle/wayland/x11/X11EglOutputTest.java
@@ -1,53 +1,54 @@
-//package org.westmalle.wayland.x11;
-//
-//import com.sun.jna.Pointer;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.mockito.Mock;
-//import org.mockito.runners.MockitoJUnitRunner;
-//import org.westmalle.wayland.nativ.libEGL.LibEGL;
-//
-//import static org.mockito.Mockito.verify;
-//
-//@RunWith(MockitoJUnitRunner.class)
-//public class X11EglOutputTest {
-//
-//    @Mock
-//    private LibEGL  libEGL;
-//    @Mock
-//    private Pointer eglDisplay;
-//    @Mock
-//    private Pointer eglSurface;
-//    @Mock
-//    private Pointer eglContext;
-//
-//    @Test
-//    public void testBegin() throws Exception {
-//        //given
-//        final X11EglOutput x11EglOutput = new X11EglOutput(this.libEGL,
-//                                                           this.eglDisplay,
-//                                                           this.eglSurface,
-//                                                           this.eglContext);
-//        //when
-//        x11EglOutput.begin();
-//        //then
-//        verify(this.libEGL).eglMakeCurrent(this.eglDisplay,
-//                                           this.eglSurface,
-//                                           this.eglSurface,
-//                                           this.eglContext);
-//    }
-//
-//    @Test
-//    public void testEnd() throws Exception {
-//        //given
-//        final X11EglOutput x11EglOutput = new X11EglOutput(this.libEGL,
-//                                                           this.eglDisplay,
-//                                                           this.eglSurface,
-//                                                           this.eglContext);
-//        //when
-//        x11EglOutput.end();
-//        //then
-//        verify(this.libEGL).eglSwapBuffers(this.eglDisplay,
-//                                           this.eglSurface);
-//    }
-//}
+package org.westmalle.wayland.x11;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.westmalle.wayland.nativ.libEGL.LibEGL;
+
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class X11EglOutputTest {
+
+    @Mock
+    private LibEGL  libEGL;
+
+    @Test
+    public void testBegin() throws Exception {
+        //given
+        final long eglDisplay = 12345;
+        final long eglSurface = 67890;
+        final long eglContext = 11235;
+
+        final X11EglOutput x11EglOutput = new X11EglOutput(this.libEGL,
+                                                           eglDisplay,
+                                                           eglSurface,
+                                                           eglContext);
+        //when
+        x11EglOutput.begin();
+        //then
+        verify(this.libEGL).eglMakeCurrent(eglDisplay,
+                                           eglSurface,
+                                           eglSurface,
+                                           eglContext);
+    }
+
+    @Test
+    public void testEnd() throws Exception {
+        //given
+        final long eglDisplay = 12345;
+        final long eglSurface = 67890;
+        final long eglContext = 11235;
+
+        final X11EglOutput x11EglOutput = new X11EglOutput(this.libEGL,
+                                                           eglDisplay,
+                                                           eglSurface,
+                                                           eglContext);
+        //when
+        x11EglOutput.end();
+        //then
+        verify(this.libEGL).eglSwapBuffers(eglDisplay,
+                                           eglSurface);
+    }
+}
