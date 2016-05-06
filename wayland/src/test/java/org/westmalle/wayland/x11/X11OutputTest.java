c7f44f8df750f3b6a776f0c1021df7cd3937112d
Erik De Rijcke <derijcke.erik@gmail.com>
2016-05-06 11:15:43 +0200

    port x11 output unit tests to jaccall

--- wayland/src/test/java/org/westmalle/wayland/x11/X11OutputTest.java
+++ wayland/src/test/java/org/westmalle/wayland/x11/X11OutputTest.java
@@ -1,61 +1,60 @@
-//package org.westmalle.wayland.x11;
-//
-//import com.sun.jna.Pointer;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.mockito.Mock;
-//import org.mockito.runners.MockitoJUnitRunner;
-//import org.westmalle.wayland.core.RenderOutput;
-//
-//import java.util.Map;
-//
-//import static com.google.common.truth.Truth.assertThat;
-//import static org.mockito.Mockito.verify;
-//
-//@RunWith(MockitoJUnitRunner.class)
-//public class X11OutputTest {
-//
-//    private final int xWindow = 12345;
-//    @Mock
-//    private X11EglOutputFactory  x11EglOutputFactory;
-//    @Mock
-//    private X11EventBus          x11EventBus;
-//    @Mock
-//    private Pointer              xcbConnection;
-//    @Mock
-//    private Pointer              xDisplay;
-//    @Mock
-//    private Map<String, Integer> atoms;
-//
-//    @Test
-//    public void testGetEglOutput() throws Exception {
-//        //given
-//        final X11Output x11Output = new X11Output(this.x11EglOutputFactory,
-//                                                  this.x11EventBus,
-//                                                  this.xcbConnection,
-//                                                  this.xDisplay,
-//                                                  this.xWindow,
-//                                                  this.atoms);
-//        //when
-//        x11Output.getEglOutput();
-//        //then
-//        verify(this.x11EglOutputFactory).create(this.xDisplay,
-//                                                this.xWindow);
-//    }
-//
-//    @Test
-//    public void testGetEglOutputInstance() throws Exception {
-//        //given
-//        final X11Output x11Output = new X11Output(this.x11EglOutputFactory,
-//                                                  this.x11EventBus,
-//                                                  this.xcbConnection,
-//                                                  this.xDisplay,
-//                                                  this.xWindow,
-//                                                  this.atoms);
-//        //when
-//        final RenderOutput renderOutput0 = x11Output.getEglOutput();
-//        final RenderOutput renderOutput1 = x11Output.getEglOutput();
-//        //then
-//        assertThat(renderOutput0).isSameAs(renderOutput1);
-//    }
-//}
+package org.westmalle.wayland.x11;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.westmalle.wayland.core.RenderOutput;
+
+import java.util.Map;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class X11OutputTest {
+
+    private final int xWindow = 12345;
+    @Mock
+    private X11EglOutputFactory  x11EglOutputFactory;
+    @Mock
+    private X11EventBus          x11EventBus;
+    @Mock
+    private Map<String, Integer> atoms;
+
+    @Test
+    public void testGetEglOutput() throws Exception {
+        //given
+        final long xcbConnection = 789326432;
+        final long xDisplay      = 483287463;
+        final X11Output x11Output = new X11Output(this.x11EglOutputFactory,
+                                                  this.x11EventBus,
+                                                  xcbConnection,
+                                                  xDisplay,
+                                                  this.xWindow,
+                                                  this.atoms);
+        //when
+        x11Output.getEglOutput();
+        //then
+        verify(this.x11EglOutputFactory).create(xDisplay,
+                                                this.xWindow);
+    }
+
+    @Test
+    public void testGetEglOutputInstance() throws Exception {
+        //given
+        final long xcbConnection = 789326432;
+        final long xDisplay      = 483287463;
+        final X11Output x11Output = new X11Output(this.x11EglOutputFactory,
+                                                  this.x11EventBus,
+                                                  xcbConnection,
+                                                  xDisplay,
+                                                  this.xWindow,
+                                                  this.atoms);
+        //when
+        final RenderOutput renderOutput0 = x11Output.getEglOutput();
+        final RenderOutput renderOutput1 = x11Output.getEglOutput();
+        //then
+        assertThat(renderOutput0).isSameAs(renderOutput1);
+    }
+}
