b1c0a3e6132a225298dc98958e1fb61d86b5f85a
Erik De Rijcke <derijcke.erik@gmail.com>
2016-06-27 15:01:12 +0200

    more gbm platform setup wip

--- wayland/src/main/java/org/westmalle/wayland/gbm/GbmPlatform.java
+++ wayland/src/main/java/org/westmalle/wayland/gbm/GbmPlatform.java
@@ -7,20 +7,45 @@ import org.westmalle.wayland.core.Renderer;
 
 import javax.annotation.Nonnull;
 
-@AutoFactory
+@AutoFactory(allowSubclasses = true,
+             className = "PrivateGbmPlatformFactory")
 public class GbmPlatform implements Platform {
 
-    GbmPlatform() {
+    private final long           drmDevice;
+    private final int            drmFd;
+    private final long           gbmDevice;
+    private final GbmConnector[] gbmConnectors;
+
+    GbmPlatform(final long drmDevice,
+                final int drmFd,
+                final long gbmDevice,
+                final GbmConnector[] gbmConnectors) {
+        this.drmDevice = drmDevice;
+        this.drmFd = drmFd;
+        this.gbmDevice = gbmDevice;
+        this.gbmConnectors = gbmConnectors;
     }
 
     @Nonnull
     @Override
-    public Connector[] getConnectors() {
-        return null;
+    public GbmConnector[] getConnectors() {
+        return this.gbmConnectors;
     }
 
     @Override
     public void accept(@Nonnull final Renderer renderer) {
+        renderer.visit(this);
+    }
 
+    public long getDrmDevice() {
+        return this.drmDevice;
+    }
+
+    public int getDrmFd() {
+        return this.drmFd;
+    }
+
+    public long getGbmDevice() {
+        return this.gbmDevice;
     }
 }
