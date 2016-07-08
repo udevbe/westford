332ca7ba1965cf41aeb06504a499ee73267ac0b1
Erik De Rijcke <Erik.De.Rijcke@prodatamobility.com>
2016-07-08 14:14:36 +0200

    add skeleton classes for html5 output

---
+++ wayland/src/main/java/org/westmalle/wayland/html5/egl/Html5EglConnector.java
@@ -0,0 +1,45 @@
+package org.westmalle.wayland.html5.egl;
+
+import com.google.auto.factory.AutoFactory;
+import org.westmalle.wayland.core.EglConnector;
+import org.westmalle.wayland.html5.Html5Connector;
+import org.westmalle.wayland.protocol.WlOutput;
+
+import javax.annotation.Nonnull;
+
+@AutoFactory(allowSubclasses = true,
+             className = "Html5EglConnectorFactory")
+public class Html5EglConnector implements EglConnector {
+
+    private final Html5Connector html5Connector;
+    private final EglConnector eglConnector;
+
+    Html5EglConnector(final Html5Connector html5Connector,
+                      final EglConnector eglConnector) {
+        this.html5Connector = html5Connector;
+        this.eglConnector = eglConnector;
+    }
+
+    @Override
+    public long getEglSurface() {
+        return this.eglConnector.getEglSurface();
+    }
+
+    @Override
+    public void begin() {
+        this.eglConnector.begin();
+    }
+
+    @Override
+    public void end() {
+        this.eglConnector.end();
+        //TODO read pixels from screen and forward to html client
+        //TODO send pixel buffer using html5Connector
+    }
+
+    @Nonnull
+    @Override
+    public WlOutput getWlOutput() {
+        return this.eglConnector.getWlOutput();
+    }
+}
