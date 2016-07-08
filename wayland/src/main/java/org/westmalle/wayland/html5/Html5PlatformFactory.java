69caa2b0f6d13af6dbe97967c3765d1ba30b0c34
Erik De Rijcke <Erik.De.Rijcke@prodatamobility.com>
2016-07-08 14:32:55 +0200

    add websocket skeleton code

--- wayland/src/main/java/org/westmalle/wayland/html5/Html5PlatformFactory.java
+++ wayland/src/main/java/org/westmalle/wayland/html5/Html5PlatformFactory.java
@@ -1,9 +1,15 @@
 package org.westmalle.wayland.html5;
 
 
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.DefaultServlet;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 import org.westmalle.wayland.core.Platform;
 
 import javax.inject.Inject;
+import java.net.URL;
+import java.util.Objects;
 
 public class Html5PlatformFactory {
 
@@ -18,7 +24,38 @@ public class Html5PlatformFactory {
     }
 
     public Html5Platform create() {
-        //TODO setup factory that inits an embedded jetty server with a websocket and pass it in the constructor
+        //TODO setup factory that properly inits an embedded jetty server with a websocket and pass it in the constructor
+
+        final Server server = new Server(8080);
+
+        final ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
+
+        // Add websocket servlet
+        final ServletHolder wsHolder = new ServletHolder("wayland", new Html5SocketServlet());
+        context.addServlet(wsHolder,"/wayland");
+
+        // Add default servlet (to serve the html/css/js)
+        // Figure out where the static files are stored.
+        //TODO write index.html & js
+        final URL urlStatics = Thread.currentThread().getContextClassLoader().getResource("index.html");
+        Objects.requireNonNull(urlStatics, "Unable to find index.html in classpath");
+        final String        urlBase   = urlStatics.toExternalForm().replaceFirst("/[^/]*$", "/");
+        final ServletHolder defHolder = new ServletHolder("default", new DefaultServlet());
+        defHolder.setInitParameter("resourceBase",urlBase);
+        defHolder.setInitParameter("dirAllowed","true");
+        context.addServlet(defHolder,"/");
+
+        try
+        {
+            server.start();
+            server.join();
+        }
+        catch (final Exception e)
+        {
+            e.printStackTrace();
+        }
 
         return this.privateHtml5PlatformFactory.create(null);
     }
---
+++ wayland/src/main/java/org/westmalle/wayland/html5/Html5Socket.java
@@ -0,0 +1,38 @@
+package org.westmalle.wayland.html5;
+
+
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.WebSocketListener;
+
+public class Html5Socket implements WebSocketListener {
+
+    //TODO implement
+
+    @Override
+    public void onWebSocketBinary(final byte[] bytes,
+                                  final int i,
+                                  final int i1) {
+
+    }
+
+    @Override
+    public void onWebSocketText(final String s) {
+
+    }
+
+    @Override
+    public void onWebSocketClose(final int i,
+                                 final String s) {
+
+    }
+
+    @Override
+    public void onWebSocketConnect(final Session session) {
+
+    }
+
+    @Override
+    public void onWebSocketError(final Throwable throwable) {
+
+    }
+}
