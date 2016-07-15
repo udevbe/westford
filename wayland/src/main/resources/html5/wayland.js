56c30fb9637ac9ac226c3100c7865409d021ca9f
Erik De Rijcke <derijcke.erik@gmail.com>
2016-07-15 16:49:54 +0200

    more client side socket handling

--- wayland/src/main/resources/html5/wayland.js
+++ wayland/src/main/resources/html5/wayland.js
@@ -4,8 +4,9 @@ var socket = new WebSocket("wss://" + location.hostname);
 socket.binaryType = "text";
 
 function createOutputCanvas(outputInfo){
-    //make canvas global
+    //make canvas & context global
     canvas = document.createElement('canvas');
+    ctx = canvas.getContext('2d');
 
     //TODO map canvas geometry to reported server output
     canvas.id     = "output";
@@ -36,22 +37,37 @@ function createOutputCanvas(outputInfo){
     }, false);
     canvas.addEventListener('keydown', function(ev) {
         //k(ey):d(own):c(ode):xxxx:t(ime):xxxxxx
-        socket.send("k:d:c"+ev.keyCode+":t:"+Date.now());
+        socket.send("k:d:c:"+ev.keyCode+":t:"+Date.now());
     }, false);
     canvas.addEventListener('keyup', function(ev) {
         //k(ey):u(p):c(ode):xxxx:t(ime):xxxxxx
-        socket.send("k:d:c"+ev.keyCode+":t:"+Date.now());
+        socket.send("k:u:c:"+ev.keyCode+":t:"+Date.now());
     }, false);
 
     document.body.appendChild(canvas);
-    //output canvas created, switch to binary mode so we can receive output frames
+
+    //output canvas created, switch to binary mode so we can receive output frames as blobs
     socket.binaryType = "blob";
+    //replace text handler with blob handler
+    socket.onmessage = function (e) {
+        //read binary data as image & put it in the canvas
+        var img = new Image;
+        img.onload = function() {
+            ctx.drawImage(img, canvas.width, canvas.height);
+        }
+        img.src = URL.createObjectURL(e.data);
+    };
     //notify server we have created the output canvas and are now ready to receive binary frames
     socket.send("ack-output-info");
 }
 
 socket.onopen = function () {
     //request output info so we can initialize our html5 canvas with the correct size
+    socket.onmessage = function (e) {
+        //TODO validate reply
+        //create canvas object based on output info reply
+        createOutputCanvas(e.data);
+    }
     socket.send("req-output-info");
 };
 
@@ -65,18 +81,3 @@ socket.onclose = function () {
     //TODO should we attempt to reconnect?
 };
 
-socket.onmessage = function (e) {
-    if(typeof e.data === "string"){
-        //TODO validate reply
-        //create canvas object based on output info reply
-        createOutputCanvas(e.data);
-    } else {
-        //read binary data as image & put it in the canvas
-        var ctx = canvas.getContext('2d');
-        var img = new Image;
-        img.onload = function() {
-            ctx.drawImage(img, canvas.width, canvas.height);
-        }
-        img.src = URL.createObjectURL(e.data);
-    }
-};
