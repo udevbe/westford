2f2cdd835c168796b7f0f151c2cf27a7ddb92f07
Erik De Rijcke <Erik.De.Rijcke@prodatamobility.com>
2016-07-25 15:14:39 +0200

    add license headers

--- wayland/src/main/java/org/westmalle/wayland/html5/Html5SocketServlet.java
+++ wayland/src/main/java/org/westmalle/wayland/html5/Html5SocketServlet.java
@@ -1,3 +1,16 @@
+//Copyright 2016 Erik De Rijcke
+//
+//Licensed under the Apache License,Version2.0(the"License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//
+//http://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing,software
+//distributed under the License is distributed on an"AS IS"BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
 package org.westmalle.wayland.html5;
 
 
@@ -8,10 +21,6 @@ import org.eclipse.jetty.websocket.servlet.ServletUpgradeResponse;
 import org.eclipse.jetty.websocket.servlet.WebSocketCreator;
 import org.eclipse.jetty.websocket.servlet.WebSocketServlet;
 import org.eclipse.jetty.websocket.servlet.WebSocketServletFactory;
-import org.westmalle.wayland.core.KeyboardDeviceFactory;
-import org.westmalle.wayland.input.LibinputXkbFactory;
-import org.westmalle.wayland.protocol.WlKeyboardFactory;
-import org.westmalle.wayland.protocol.WlSeatFactory;
 
 import javax.annotation.Nonnull;
 
