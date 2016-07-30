7ab4d10b86542bae927c30f65631f7ee222c2140
Erik De Rijcke <derijcke.erik@gmail.com>
2016-07-30 22:24:11 +0200

    disable/enable input when we leave/enter a vt

--- bootstrap/src/main/java/org/westmalle/wayland/bootstrap/dispmanx/DispmanxEglCompositor.java
+++ bootstrap/src/main/java/org/westmalle/wayland/bootstrap/dispmanx/DispmanxEglCompositor.java
@@ -6,13 +6,15 @@ import org.westmalle.wayland.core.LifeCycle;
 import org.westmalle.wayland.dispmanx.egl.DispmanxEglPlatformModule;
 import org.westmalle.wayland.gles2.Gles2RendererModule;
 import org.westmalle.wayland.input.LibinputSeatFactory;
+import org.westmalle.wayland.tty.TtyModule;
 
 import javax.inject.Singleton;
 
 @Singleton
 @Component(modules = {CoreModule.class,
                       Gles2RendererModule.class,
-                      DispmanxEglPlatformModule.class})
+                      DispmanxEglPlatformModule.class,
+                      TtyModule.class})
 public interface DispmanxEglCompositor {
     LifeCycle lifeCycle();
 
--- wayland/src/main/java/org/westmalle/wayland/input/LibinputSeat.java
+++ wayland/src/main/java/org/westmalle/wayland/input/LibinputSeat.java
@@ -16,27 +16,20 @@ package org.westmalle.wayland.input;
 import com.google.auto.factory.AutoFactory;
 import com.google.auto.factory.Provided;
 import org.freedesktop.jaccall.Pointer;
-import org.freedesktop.jaccall.Ptr;
 import org.freedesktop.wayland.server.Display;
+import org.freedesktop.wayland.server.EventSource;
 import org.freedesktop.wayland.server.jaccall.WaylandServerCore;
 import org.freedesktop.wayland.shared.WlSeatCapability;
-import org.westmalle.wayland.core.Compositor;
 import org.westmalle.wayland.core.Seat;
-import org.westmalle.wayland.nativ.libc.Libc;
 import org.westmalle.wayland.nativ.libinput.Libinput;
-import org.westmalle.wayland.nativ.libinput.libinput_interface;
-import org.westmalle.wayland.nativ.libudev.Libudev;
-import org.westmalle.wayland.protocol.WlOutput;
 import org.westmalle.wayland.protocol.WlSeat;
 
 import javax.annotation.Nonnull;
-
 import java.util.EnumSet;
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
 
-import static org.freedesktop.jaccall.Pointer.malloc;
 import static org.freedesktop.jaccall.Pointer.wrap;
 import static org.freedesktop.wayland.shared.WlSeatCapability.KEYBOARD;
 import static org.freedesktop.wayland.shared.WlSeatCapability.POINTER;
@@ -56,8 +49,6 @@ import static org.westmalle.wayland.nativ.libinput.Libinput.LIBINPUT_EVENT_TOUCH
 import static org.westmalle.wayland.nativ.libinput.Libinput.LIBINPUT_EVENT_TOUCH_FRAME;
 import static org.westmalle.wayland.nativ.libinput.Libinput.LIBINPUT_EVENT_TOUCH_MOTION;
 import static org.westmalle.wayland.nativ.libinput.Libinput.LIBINPUT_EVENT_TOUCH_UP;
-import static org.westmalle.wayland.nativ.libinput.Pointerclose_restricted.nref;
-import static org.westmalle.wayland.nativ.libinput.Pointeropen_restricted.nref;
 
 @AutoFactory(allowSubclasses = true,
              className = "PrivateLibinputSeatFactory")
@@ -66,93 +57,41 @@ public class LibinputSeat {
     @Nonnull
     private final Display               display;
     @Nonnull
-    private final Libudev               libudev;
-    @Nonnull
     private final Libinput              libinput;
     @Nonnull
-    private final Libc                  libc;
-    @Nonnull
     private final LibinputDeviceFactory libinputDeviceFactory;
+    private final long                  libinputContext;
     @Nonnull
     private final WlSeat                wlSeat;
 
-    private final Set<LibinputDevice> libinputDevices = new HashSet<>();
+    private final Set<LibinputDevice>   libinputDevices  = new HashSet<>();
+    private       Optional<EventSource> inputEventSource = Optional.empty();
 
     LibinputSeat(@Provided @Nonnull final Display display,
-                 @Provided @Nonnull final Libudev libudev,
                  @Provided @Nonnull final Libinput libinput,
-                 @Provided @Nonnull final Libc libc,
                  @Provided @Nonnull final LibinputDeviceFactory libinputDeviceFactory,
+                 final long libinputContext,
                  @Nonnull final WlSeat wlSeat) {
         this.display = display;
-        this.libudev = libudev;
         this.libinput = libinput;
-        this.libc = libc;
         this.libinputDeviceFactory = libinputDeviceFactory;
+        this.libinputContext = libinputContext;
         this.wlSeat = wlSeat;
     }
 
-    //TODO unit test all possible events that can occur after call to open
-    public void open(final String seat) {
-        final long libinput = createUdevContext(seat);
-        loop(libinput);
-        //TODO set/update seat capabilities?
-    }
-
-    private long createUdevContext(final String seatId) {
-        final long udev = this.libudev.udev_new();
-        if (udev == 0L) {
-            throw new RuntimeException("Failed to initialize udev");
-        }
-
-        final Pointer<libinput_interface> interface_ = malloc(libinput_interface.SIZE,
-                                                              libinput_interface.class);
-        interface_.dref()
-                  .open_restricted(nref(this::openRestricted));
-        interface_.dref()
-                  .close_restricted(nref(this::closeRestricted));
-
-        final long libinput = this.libinput.libinput_udev_create_context(interface_.address,
-                                                                         0,
-                                                                         udev);
-
-        if (this.libinput.libinput_udev_assign_seat(libinput,
-                                                    Pointer.nref(seatId).address) != 0) {
-            this.libinput.libinput_unref(libinput);
-            this.libudev.udev_unref(udev);
-
-            throw new RuntimeException(String.format("Failed to set seat=%s",
-                                                     seatId));
-        }
-
-        return libinput;
-    }
-
-    private int openRestricted(@Ptr(String.class) final long path,
-                               final int flags,
-                               @Ptr(Void.class) final long user_data) {
-        final int fd = this.libc.open(path,
-                                      flags);
-
-        return fd < 0 ? -this.libc.getErrno() : fd;
-    }
-
-    private void closeRestricted(final int fd,
-                                 @Ptr(Void.class) final long user_data) {
-        this.libc.close(fd);
-    }
-
     private void loop(final long libinput) {
-        final int libinputFd = this.libinput.libinput_get_fd(libinput);
-        this.display.getEventLoop()
-                    .addFileDescriptor(libinputFd,
-                                       WaylandServerCore.WL_EVENT_READABLE,
-                                       (fd, mask) -> {
-                                           if (fd == libinputFd) {
-                                               processEvents(libinput);
-                                           }
-                                           return 0;
-                                       });
+        if (!this.inputEventSource.isPresent()) {
+            final int libinputFd = this.libinput.libinput_get_fd(libinput);
+            this.inputEventSource = Optional.of(this.display.getEventLoop()
+                                                            .addFileDescriptor(libinputFd,
+                                                                               WaylandServerCore.WL_EVENT_READABLE,
+                                                                               (fd, mask) -> {
+                                                                                   if (fd == libinputFd) {
+                                                                                       processEvents(libinput);
+                                                                                   }
+                                                                                   return 0;
+                                                                               }));
+        }
     }
 
     private void processEvents(final long libinput) {
@@ -296,5 +235,13 @@ public class LibinputSeat {
         final Seat seat = this.wlSeat.getSeat();
         seat.setCapabilities(seatCapabilities);
         seat.emitCapabilities(this.wlSeat.getResources());
+    }
+
+    public void disableInput() {
+        this.inputEventSource.ifPresent(EventSource::remove);
+    }
+
+    public void enableInput() {
+        loop(this.libinputContext);
     }
 }
