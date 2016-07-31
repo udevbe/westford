3406a206d12b5624e8d6062d0f1f94f6b5e303ba
Erik De Rijcke <DeRijcke.Erik@gmail.com>
2016-07-31 21:17:12 +0200

    disable/enable rendering when we leave/enter a vt

--- wayland/src/main/java/org/westmalle/wayland/nativ/linux/Stat.java
+++ wayland/src/main/java/org/westmalle/wayland/nativ/linux/Stat.java
@@ -1,5 +1,9 @@
 package org.westmalle.wayland.nativ.linux;
 
 public class Stat {
+
+    public static final int _STAT_VER_LINUX = 3;
+    public static final int _STAT_VER       = _STAT_VER_LINUX;
+
     public static final short KDSKBMUTE = 0x4B51;
 }
