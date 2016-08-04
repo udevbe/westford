c8281beaed87b2f7969adfd6227fe00e5d29075f
Erik De Rijcke <derijcke.erik@gmail.com>
2016-08-04 15:33:13 +0200

    add some basic help & argument parsing

--- bootstrap/src/main/java/org/westmalle/wayland/bootstrap/Launcher.java
+++ bootstrap/src/main/java/org/westmalle/wayland/bootstrap/Launcher.java
@@ -11,7 +11,7 @@ import javax.annotation.Nonnull;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -22,7 +22,22 @@ import java.util.Objects;
  * (like the gc thread) could receive the native signal, and fire the default handler (which will exit the program).
  */
 public class Launcher {
+
+    private static final String optionPrefix = "option=";
+
     public static void main(final String[] args) throws IOException, InterruptedException {
+
+        if (args.length == 1 &&
+            (args[0].equals("--help") || args[0].equals("-h"))) {
+            System.out.println("This program changes the native signal mask before launching a new jvm.\n" +
+                               "This ensures that we can catch the correct native signal in the child jvm thread of our choosing.\n" +
+                               "If we did not change the signal mask, any thread out of our control (like the gc thread)\n" +
+                               "could receive the native signal, and fire the default handler (which would exit the program).\n\n" +
+                               "Usage:\n" +
+                               "\t option=<arg> \t - Pass <arg> as a child jvm option, eg. option=-Dkey=value or option=-Xmx=1234m\n" +
+                               "\t <args> \t - Pass <args> as-is to the child jvm as program arguments.");
+            System.exit(0);
+        }
 
         new Libc_Symbols().link();
         new Libpthread_Symbols().link();
@@ -39,19 +54,31 @@ public class Launcher {
                                    Pointer.ref(sigset).address,
                                    0L);
 
-        System.exit(startNewJavaProcess("",
+        final List<String> options     = new LinkedList<>();
+        final List<String> programArgs = new LinkedList<>();
+
+        for (final String arg : args) {
+            if (arg.startsWith(optionPrefix)) {
+                options.add(arg.substring(optionPrefix.length()));
+            }
+            else {
+                programArgs.add(arg);
+            }
+        }
+
+        System.exit(startNewJavaProcess(options,
                                         Boot.class.getName(),
-                                        args).waitFor());
+                                        programArgs).waitFor());
     }
 
-    private static Process startNewJavaProcess(@Nonnull final String optionsAsString,
+    private static Process startNewJavaProcess(@Nonnull final List<String> options,
                                                @Nonnull final String mainClass,
-                                               @Nonnull final String[] arguments) throws IOException {
-        Objects.requireNonNull(optionsAsString);
+                                               @Nonnull final List<String> arguments) throws IOException {
+        Objects.requireNonNull(options);
         Objects.requireNonNull(mainClass);
         Objects.requireNonNull(arguments);
 
-        final ProcessBuilder processBuilder = createProcess(optionsAsString,
+        final ProcessBuilder processBuilder = createProcess(options,
                                                             mainClass,
                                                             arguments);
         processBuilder.inheritIO();
@@ -59,20 +86,21 @@ public class Launcher {
     }
 
 
-    private static ProcessBuilder createProcess(final String optionsAsString,
+    private static ProcessBuilder createProcess(final List<String> options,
                                                 final String mainClass,
-                                                final String[] arguments) {
+                                                final List<String> arguments) {
         final String jvm       = System.getProperty("java.home") + File.separator + "bin" + File.separator + "java";
         final String classpath = System.getProperty("java.class.path");
 
         final List<String> command = new ArrayList<>();
         command.add(jvm);
-        if (!optionsAsString.isEmpty()) {
-            final String[] options = optionsAsString.split(" ");
-            command.addAll(Arrays.asList(options));
+        if (!options.isEmpty()) {
+            command.addAll(options);
         }
         command.add(mainClass);
-        command.addAll(Arrays.asList(arguments));
+        if (!arguments.isEmpty()) {
+            command.addAll(arguments);
+        }
 
         final ProcessBuilder      processBuilder = new ProcessBuilder(command);
         final Map<String, String> environment    = processBuilder.environment();
