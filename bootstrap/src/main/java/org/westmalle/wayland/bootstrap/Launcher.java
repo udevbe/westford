3524f90c4fd9857d3a87e497a65fa8fa32777006
Erik De Rijcke <Erik.De.Rijcke@prodatamobility.com>
2016-08-04 13:27:42 +0200

    use parent process to properly set signal mask

---
+++ bootstrap/src/main/java/org/westmalle/wayland/bootstrap/Launcher.java
@@ -0,0 +1,83 @@
+package org.westmalle.wayland.bootstrap;
+
+import org.freedesktop.jaccall.Pointer;
+import org.westmalle.wayland.nativ.glibc.Libc;
+import org.westmalle.wayland.nativ.glibc.Libc_Symbols;
+import org.westmalle.wayland.nativ.glibc.Libpthread;
+import org.westmalle.wayland.nativ.glibc.Libpthread_Symbols;
+import org.westmalle.wayland.nativ.glibc.sigset_t;
+
+import javax.annotation.Nonnull;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * This class changes the native signal mask before launching a new jvm. This ensures that we can catch the correct native
+ * signal in the thread of our choosing. If we did not change the signal mask, any thread out of our control
+ * (like the gc thread) could receive the native signal, and fire the default handler (which will exit the program).
+ */
+public class Launcher {
+    public static void main(final String[] args) throws IOException, InterruptedException {
+
+        new Libc_Symbols().link();
+        new Libpthread_Symbols().link();
+
+        final Libc       libc       = new Libc();
+        final Libpthread libpthread = new Libpthread();
+
+        //we block the signals that we want to catch in our child process.
+        final sigset_t sigset = new sigset_t();
+        libpthread.sigemptyset(Pointer.ref(sigset).address);
+        libpthread.sigaddset(Pointer.ref(sigset).address,
+                             libc.SIGRTMIN());
+        libpthread.pthread_sigmask(Libc.SIG_BLOCK,
+                                   Pointer.ref(sigset).address,
+                                   0L);
+
+        System.exit(startNewJavaProcess("",
+                                        Boot.class.getName(),
+                                        args).waitFor());
+    }
+
+    private static Process startNewJavaProcess(@Nonnull final String optionsAsString,
+                                               @Nonnull final String mainClass,
+                                               @Nonnull final String[] arguments) throws IOException {
+        Objects.requireNonNull(optionsAsString);
+        Objects.requireNonNull(mainClass);
+        Objects.requireNonNull(arguments);
+
+        final ProcessBuilder processBuilder = createProcess(optionsAsString,
+                                                            mainClass,
+                                                            arguments);
+        processBuilder.inheritIO();
+        return processBuilder.start();
+    }
+
+
+    private static ProcessBuilder createProcess(final String optionsAsString,
+                                                final String mainClass,
+                                                final String[] arguments) {
+        final String jvm       = System.getProperty("java.home") + File.separator + "bin" + File.separator + "java";
+        final String classpath = System.getProperty("java.class.path");
+
+        final List<String> command = new ArrayList<>();
+        command.add(jvm);
+        if (!optionsAsString.isEmpty()) {
+            final String[] options = optionsAsString.split(" ");
+            command.addAll(Arrays.asList(options));
+        }
+        command.add(mainClass);
+        command.addAll(Arrays.asList(arguments));
+
+        final ProcessBuilder      processBuilder = new ProcessBuilder(command);
+        final Map<String, String> environment    = processBuilder.environment();
+        environment.put("CLASSPATH",
+                        classpath);
+        return processBuilder;
+    }
+}
