da11fb10b17ef648d2a16ac71e1f0b17807b024c
Erik De Rijcke <derijcke.erik@gmail.com>
2016-08-05 11:35:55 +0200

    change license to agpl3

--- wayland/src/main/resources/html5/wayland.js
+++ wayland/src/main/resources/html5/wayland.js
@@ -1,3 +1,29 @@
+/**
+ *
+ * @licstart  The following is the entire license notice for the
+ *  JavaScript code in this page.
+ *
+ * Westmalle Wayland Compositor.
+ * Copyright (C) 2016  Erik De Rijcke
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @licend  The above is the entire license notice
+ * for the JavaScript code in this page.
+ *
+ */
+
 //use browser url to find socket to connect to
 var parser = document.createElement('a');
 parser.href = document.URL;
@@ -97,4 +123,3 @@ socket.onerror = function (error) {
 socket.onclose = function (event) {
     //TODO should we attempt to reconnect?
 };
-
