080a22443f1e055b49931d28748c0307ea29f6f8
Erik De Rijcke <derijcke.erik@gmail.com>
2016-08-31 21:28:46 +0200

    make privileges and launchers singletons

--- wayland/src/main/java/org/westmalle/launch/direct/DirectModule.java
+++ wayland/src/main/java/org/westmalle/launch/direct/DirectModule.java
@@ -7,12 +7,15 @@ import org.westmalle.launch.Privileges;
 import org.westmalle.nativ.NativeModule;
 import org.westmalle.tty.TtyModule;
 
+import javax.inject.Singleton;
+
 @Module(includes = {
         TtyModule.class,
         NativeModule.class
 })
 public class DirectModule {
 
+    @Singleton
     @Provides
     Privileges providePrivileges(final DirectPrivilegesFactory directPrivilegesFactory) {
         return directPrivilegesFactory.create();
