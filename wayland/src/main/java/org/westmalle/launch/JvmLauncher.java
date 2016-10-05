1f057878c9fe282c109a968dbc5f311754e37ea4
Erik De Rijcke <derijcke.erik@gmail.com>
2016-10-05 22:51:10 +0200

    pass fds through env vars & enable debug for child processes by default

--- wayland/src/main/java/org/westmalle/launch/JvmLauncher.java
+++ wayland/src/main/java/org/westmalle/launch/JvmLauncher.java
@@ -10,43 +10,46 @@ import java.util.Map;
 
 public class JvmLauncher {
 
+    private static final String DEBUG_COMMAND = "-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=%d";
+    private static       int    DEBUG_PORT    = 6000;
+
     @Nonnull
     public Process fork(@Nonnull final String mainClassName) throws IOException, InterruptedException {
-        return startNewJavaProcess(Collections.emptyList(),
+        return startNewJavaProcess(Collections.emptyMap(),
                                    mainClassName);
     }
 
-    private Process startNewJavaProcess(final List<String> options,
+    private Process startNewJavaProcess(final Map<String, String> environment,
                                         final String mainClass) throws IOException {
-        final ProcessBuilder processBuilder = createProcess(options,
+        final ProcessBuilder processBuilder = createProcess(environment,
                                                             mainClass);
         processBuilder.inheritIO();
         return processBuilder.start();
     }
 
-    private ProcessBuilder createProcess(final List<String> options,
+    private ProcessBuilder createProcess(final Map<String, String> environmentExtra,
                                          final String mainClass) {
         final String jvm       = System.getProperty("java.home") + File.separator + "bin" + File.separator + "java";
         final String classpath = System.getProperty("java.class.path");
 
         final List<String> command = new ArrayList<>();
         command.add(jvm);
-        if (!options.isEmpty()) {
-            command.addAll(options);
-        }
+        command.add(String.format(DEBUG_COMMAND,
+                                  ++DEBUG_PORT));
         command.add(mainClass);
 
         final ProcessBuilder      processBuilder = new ProcessBuilder(command);
         final Map<String, String> environment    = processBuilder.environment();
+        environment.putAll(environmentExtra);
         environment.put("CLASSPATH",
                         classpath);
         return processBuilder;
     }
 
     @Nonnull
-    public Process fork(@Nonnull final List<String> options,
+    public Process fork(@Nonnull final Map<String, String> environmentExtra,
                         @Nonnull final String mainClassName) throws IOException, InterruptedException {
-        return startNewJavaProcess(options,
+        return startNewJavaProcess(environmentExtra,
                                    mainClassName);
     }
 
