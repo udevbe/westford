9c25df529a36ed63d33421d47a552229a35cbae3
Erik De Rijcke <derijcke.erik@gmail.com>
2016-10-27 16:26:58 +0200

    implement native launcher code for indirect drm access (c/p from direct drm)

---
+++ bootstrap/drm.indirect/src/main/c/drmindirect/src/drmindirect.c
@@ -0,0 +1,82 @@
+//TODO launch child process and communicate with a unix socket
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <signal.h>
+
+#include <jni.h>
+
+void setup_signals(void){
+    sigset_t sigset;
+    sigemptyset(&sigset);
+    //the signals being blocked here must the same signals defined in the org.westmalle.tty.Tty class.
+
+    sigaddset(&sigset,
+              SIGRTMIN);
+    sigaddset(&sigset,
+              SIGRTMIN + 1);
+    if(-1 == sigprocmask(SIG_BLOCK,
+                         &sigset,
+                         NULL)){
+        fprintf(stderr, "ERROR: Failed to block SIGRTMIN and SIGRTMIN+1 signals.\n");
+	    exit(1);
+    }
+}
+
+JNIEnv* create_vm(int argc, char **argv) {
+	JavaVM* jvm;
+	JNIEnv* env;
+	JavaVMInitArgs args;
+	JavaVMOption options[argc+1];
+	int i;
+
+	args.version = JNI_VERSION_1_6;
+	args.nOptions = argc;
+
+	options[0].optionString = "-Djava.class.path=drm.indirect.jar";
+    fprintf(stdout, "INFO: Adding JVM option %s.\n", options[0].optionString);
+
+    for(i = 1; i < argc; i++) {
+        options[i].optionString = argv[i];
+        fprintf(stdout, "INFO: Adding JVM option %s.\n", argv[i]);
+    }
+
+	args.options = options;
+	args.ignoreUnrecognized = JNI_FALSE;
+
+	if(JNI_CreateJavaVM(&jvm, (void **)&env, &args)){
+        fprintf(stderr, "ERROR: Failed to create the JVM.\n");
+	    exit(1);
+	}
+
+	return env;
+}
+
+void invoke_class(JNIEnv* env) {
+	jclass bootClass;
+	jmethodID mainMethod;
+	const char* cls = "org/westmalle/wayland/bootstrap/drm/indirect/Boot";
+	const char* entry = "main_from_native";
+
+	bootClass = (*env)->FindClass(env, cls);
+	if(bootClass){
+		mainMethod = (*env)->GetStaticMethodID(env, bootClass, entry, "()V");
+	} else {
+	    fprintf(stderr, "ERROR: %s not found on classpath.\n", cls);
+        exit(1);
+	}
+
+    if(mainMethod){
+        (*env)->CallStaticVoidMethod(env, bootClass, mainMethod);
+    } else {
+    	fprintf(stderr, "ERROR: Method %s not found on class %s.\n", entry, cls);
+    	exit(1);
+    }
+}
+
+int main(int argc, char **argv) {
+    setup_signals();
+	JNIEnv* env = create_vm(argc, argv);
+	invoke_class(env);
+	return 0;
+}
