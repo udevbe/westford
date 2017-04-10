ef06c130549031786703ba3b0361d36a7cb587a0
Erik De Rijcke <Erik.De.Rijcke@bar.com>
2017-04-10 17:18:08 +0200

    drm event context is already at 2

--- compositor/src/main/java/org/westford/compositor/drm/DrmEventBusFactory.java
+++ compositor/src/main/java/org/westford/compositor/drm/DrmEventBusFactory.java
@@ -45,7 +45,7 @@ public class DrmEventBusFactory {
                                                                               drmEventContextP.address);
 
         final DrmEventContext drmEventContext = drmEventContextP.dref();
-        drmEventContext.version(2);
+        drmEventContext.version(Libdrm.DRM_EVENT_CONTEXT_VERSION);
         drmEventContext.page_flip_handler(Pointerpage_flip_handler.nref(drmEventBus::pageFlipHandler));
         drmEventContext.vblank_handler(Pointervblank_handler.nref(drmEventBus::vblankHandler));
 
